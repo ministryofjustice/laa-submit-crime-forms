ceb17b94b8c6210a6ddc6cf3370760fb3ce72903
David Henry <david.henry@digital.justice.gov.uk>
2023-04-21 11:25:50 +0100

    Start on Form for Firm details

--- app/forms/steps/firm_details_form.rb
+++ app/forms/steps/firm_details_form.rb
@@ -2,7 +2,29 @@ require 'steps/base_form_object'
 
 module Steps
   class FirmDetailsForm < Steps::BaseFormObject
+    attr_accessor :firm_office_attributes
+    attr_accessor :solicitor_attributes
+
+    def firm_office
+      @firm_office ||= FirmOfficeForm.new(firm_office_fields, application: application)
+    end
+
+    def solicitor
+      @solicitor ||= SolicitorForm.new(solicitor_fields, application: application)
+    end
+
     private
+
+    def firm_office_fields
+      # use attribute hash for values from User,
+      # object to lookup existing values
+      # default to blank (maybe replace with most recent details in the future)
+      firm_office_attributes || application.firm_office || {}
+    end
+
+    def solicitor_fields
+      solicitor_attributes || application.solicitor || {}
+    end
 
     def persist!
       true
