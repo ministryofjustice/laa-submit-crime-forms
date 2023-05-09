01877443bdb5ad4d65142bf54d0d879c9c672c29
David Henry <david.henry@digital.justice.gov.uk>
2023-05-09 09:58:51 +0100

    Populate other start page items

--- app/presenters/start_page_task_list.rb
+++ app/presenters/start_page_task_list.rb
@@ -1,5 +1,10 @@
 class StartPageTaskList < TaskList::Collection
   SECTIONS = [
-    [:about_you, [:firm_details]],
+    [:about_you, [:firm_details, :claim_reason]],
+    [:case, [:case_details, :case_disposal, :hearing_details]],
+    [:defendant, [:defendant_details]],
+    [:claim, [:claim_justification, :claim_details, :work_items, :letters_calls, :disbursements, :claim_summary, :additional_info]],
+    [:evidence, [:upload_evidence]],
+    [:review, [:check_answers]]
   ]
 end
--- spec/steps/start_page/integration_spec.rb
+++ spec/steps/start_page/integration_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'User can see an application status', type: :system do
   it 'can do green path' do
     visit steps_start_page_path(claim.id)
 
-    within('.moj-task-list__item', text: 'Firm details') do
+    within('.moj-task-list__item', text: 'Your details') do
       expect(page).to have_content('In progress')
     end
   end
