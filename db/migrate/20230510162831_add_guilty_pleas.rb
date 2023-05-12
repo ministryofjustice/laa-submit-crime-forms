a9cc14caeec7c0644beb516d27c6e0ce432e041b
David Henry <david.henry@digital.justice.gov.uk>
2023-05-12 14:39:10 +0100

    Fix migration

--- db/migrate/20230510162831_add_guilty_pleas.rb
+++ db/migrate/20230510162831_add_guilty_pleas.rb
@@ -1,9 +1,9 @@
 class AddGuiltyPleas < ActiveRecord::Migration[7.0]
   def change
+    remove_column :claims, :plea, :boolean
+
     add_column :claims, :plea, :string
     add_column :claims, :arrest_warrent_date, :date
     add_column :claims, :cracked_trial_date, :date
-
-    remove_column :claims, :plea
   end
 end
