8fb7a85ed31e91e3a4d914e0bc22e7e69e9f7933
sandeepmittaltecharchitect <123737876+sandeepmittaltecharchitect@users.noreply.github.com>
2023-05-18 16:43:02 +0100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.hosts << crm7dev.apps.live.cloud-platform.service.justice.gov.uk
 
   # Do not eager load code on boot.
   config.eager_load = false
