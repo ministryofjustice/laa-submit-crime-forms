2d41490db2a2113102833f9f59ad657064d01947
David Henry <david.henry@digital.justice.gov.uk>
2023-05-23 12:30:18 +0100

    Add tests to engine

--- gems/laa_multi_step_forms/app/validators/nested_validator.rb
+++ gems/laa_multi_step_forms/app/validators/nested_validator.rb
@@ -1,31 +1,36 @@
 class NestedValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    value.each.with_index do |sub_record, index|
-      add_indexed_errors(record, attribute, sub_record, index) unless sub_record.valid?
+  def validate_each(object, attribute, value)
+    return unless value
+
+    was_array = value.is_a?(Array)
+
+    Array(value).each.with_index do |sub_record, index|
+      add_indexed_errors(object, attribute, was_array, sub_record, index) unless sub_record.valid?
     end
   end
 
   private
 
-  def add_indexed_errors(record, attribute, sub_record, index)
+  def add_indexed_errors(object, attribute, was_array, sub_record, index)
     sub_record.errors.each do |error|
-      attr_name = indexed_attribute(index, attribute, error.attribute)
+      attr_name = indexed_attribute(index, attribute, was_array, error.attribute)
 
-      record.errors.add(
+      object.errors.add(
         attr_name,
         error.type,
-        message: error_message(sub_record, error), name: error_message_name(record, index)
+        message: error_message(sub_record, error), name: error_message_name(object, index)
       )
 
       # We define the attribute getter as it doesn't really exist
-      record.define_singleton_method(attr_name) do
+      object.define_singleton_method(attr_name) do
         sub_record.public_send(error.attribute)
       end
     end
   end
 
-  def indexed_attribute(index, attribute, attr)
-    "#{attribute}-attributes[#{index}].#{attr}"
+  def indexed_attribute(index, attribute, was_array, attr)
+    return "#{attribute}-attributes[#{index}].#{attr}" if was_array
+    "#{attribute}-attributes.#{attr}"
   end
 
   # `activemodel.errors.models.steps/case/offence_date_fieldset_form.summary.x.y`
@@ -36,9 +41,9 @@ class NestedValidator < ActiveModel::EachValidator
     )
   end
 
-  def error_message_name(record, index)
+  def error_message_name(object, index)
     if options[:name]
-      record.method(options[:name]).call(index)
+      object.method(options[:name]).call(index)
     else
       index + 1
     end
