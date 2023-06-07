46f59ba812c61ba36c10fb5e0077efdc1ed78d99
mhunt3142 <128395220+mhunt3142@users.noreply.github.com>
2023-06-07 09:16:50 +0100

    failing test - revert

--- app/forms/steps/claim_details_form.rb
+++ app/forms/steps/claim_details_form.rb
@@ -13,12 +13,8 @@ module Steps
     validates :prosecution_evidence, presence: true
     validates :defence_statement, presence: true
     validates :number_of_witnesses, presence: true, numericality: { only_integer: true, greater_than: 0 }
-    validates :time_spent_hours, numericality: { only_integer: true, greater_than: 0 }, if: lambda do |form|
-      form.preparation_time == YesNoAnswer::YES
-    end
-    validates :time_spent_mins, numericality: { only_integer: true, greater_than: 0 }, if: lambda do |form|
-      form.preparation_time == YesNoAnswer::YES
-    end
+    validates :time_spent_hours, numericality: { only_integer: true, greater_than: 0 }, if: lambda do |form| { nform.preparation_time == YesNoAnswer::YES }
+    validates :time_spent_mins, numericality: { only_integer: true, greater_than: 0 }, if: lambda do |form| { form.preparation_time == YesNoAnswer::YES }
 
     BOOLEAN_FIELDS.each do |field|
       validates field, presence: true, inclusion: { in: YesNoAnswer.values }
