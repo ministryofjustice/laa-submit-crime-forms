1212b94aac9838bc516e81edc38a9c9ec4360821
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2023-06-13 09:57:13 +0100

    Further optimising the JS controller

--- app/javascript/controllers/letters_calls_form_controller.js
+++ app/javascript/controllers/letters_calls_form_controller.js
@@ -1,3 +1,78 @@
+// Get the value of an element attribute as a number or a default value
+function getAttributeAsNumber(element, attributeName, defaultValue = 0) {
+	return parseFloat(element.getAttribute(attributeName)) || defaultValue;
+}
+
+// Calculate the total based on the values of letters and phone_calls inputs
+function calculateTotal(lettersValue, phoneCallsValue, unitPriceForLetters, unitPriceForPhoneCalls) {
+	const total = lettersValue * unitPriceForLetters + phoneCallsValue * unitPriceForPhoneCalls;
+	return parseFloat(total.toFixed(2)); // Format total to two decimal places
+}
+
+// Calculate the total with uplift
+function calculateTotalWithUplift(currentTotal, upliftPercentage) {
+	const upliftToAdd = currentTotal * (upliftPercentage / 100);
+	const totalWithUplift = (currentTotal + upliftToAdd);
+	return totalWithUplift;
+}
+
+// Update the DOM element with the provided total
+function updateDomElementTotal(totalAmount, element) {
+	totalAmount = parseFloat(totalAmount).toFixed(2);
+	element.innerHTML = '£' + totalAmount;
+}
+
+// Check if all necessary field values exist
+function checkIfAllFieldValueExists(lettersValue, phoneCallsValue) {
+	return lettersValue && phoneCallsValue;
+}
+
+// Handle change events on the form
+export function handleFormChange(event, form, totalElement, lettersElement, phoneCallsElement, upliftCheckboxElement, upliftPercentageElement) {
+	const lettersValue = parseFloat(lettersElement.value) || 0;
+	const phoneCallsValue = parseFloat(phoneCallsElement.value) || 0;
+	const unitPriceForLetters = getAttributeAsNumber(lettersElement, 'data-rate-letters');
+	const unitPriceForPhoneCalls = getAttributeAsNumber(phoneCallsElement, 'data-rate-phone-calls');
+	const upliftPercentage = parseFloat(upliftPercentageElement.value) || 0;
+
+	if (checkIfAllFieldValueExists(lettersValue, phoneCallsValue)) {
+		const currentTotal = calculateTotal(lettersValue, phoneCallsValue, unitPriceForLetters, unitPriceForPhoneCalls);
+		// Triggered when letters or phone calls value changes, or when uplift percentage changes
+		if ([lettersElement, phoneCallsElement, upliftPercentageElement].includes(event.target)) {
+			// Update total
+			const totalWithUplift = calculateTotalWithUplift(currentTotal, upliftPercentage);
+			updateDomElementTotal(totalWithUplift, totalElement);
+		}
+		if (event.target === upliftCheckboxElement && !upliftCheckboxElement.checked) {
+			// Clear uplift percentage value
+			upliftPercentageElement.value = '';
+			// Update total
+			const totalWithUplift = calculateTotalWithUplift(currentTotal, 0);
+			updateDomElementTotal(totalWithUplift, totalElement);
+		}
+	}
+}
+
+// Attach event listener to the form for change events
+function initializeForm(
+	form,
+	totalElement,
+	lettersElement,
+	phoneCallsElement,
+	upliftCheckboxElement,
+	upliftPercentageElement
+) {
+	form.addEventListener('change', (event) => handleFormChange(
+		event,
+		form,
+		totalElement,
+		lettersElement,
+		phoneCallsElement,
+		upliftCheckboxElement,
+		upliftPercentageElement
+	));
+}
+
 const form = document.getElementById('new_steps_letters_calls_form');
 const total_for_letters_and_phone_calls = document.getElementById('letters-calls-total');
 const letters = document.getElementById('steps-letters-calls-form-letters-field');
@@ -5,50 +80,14 @@ const phone_calls = document.getElementById('steps-letters-calls-form-calls-fiel
 const letter_calls_uplift_checkbox = document.getElementById('steps-letters-calls-form-apply-uplift-true-field');
 const letter_calls_uplift_percentage = document.getElementById('steps-letters-calls-form-letters-calls-uplift-field');
 
-// Calculate the total based on the values of letters and phone_calls inputs
-function calculateTotal() {
-    const lettersValue = parseFloat(letters.value) || 0;
-    const phoneCallsValue = parseFloat(phone_calls.value) || 0;
-    const unitPriceForLetters = parseFloat(letters.getAttribute('data-rate-letters')) || 0;
-    const unitPriceForPhoneCalls = parseFloat(phone_calls.getAttribute('data-rate-phone-calls')) || 0;
-    const total = (lettersValue * unitPriceForLetters) + (phoneCallsValue * unitPriceForPhoneCalls);
-    return total.toFixed(2); // Format total to two decimal places
-}
-
-// Calculate the total with uplift and update the DOM element accordingly
-export function calculateTotalWithUplift() {
-    const currentTotal = parseFloat(calculateTotal()) || 0;
-    const upliftPercentage = parseFloat(letter_calls_uplift_percentage.value) || 0;
-    const upliftToAdd = currentTotal * (upliftPercentage / 100);
-    const totalWithUplift = (currentTotal + upliftToAdd).toFixed(2); // Format total to two decimal places
-    return totalWithUplift;
-}
-
-// Update the DOM element with the provided total, or calculate the total if not provided
-function updateDomElementTotal() {
-    const totalAmount = calculateTotalWithUplift();
-    total_for_letters_and_phone_calls.innerHTML = '£' + totalAmount;
-}
-
-// Check if all necessary field values exist
-function checkIfAllFieldValueExists() {
-    return letters.value && phone_calls.value;
-}
-
-// Handle change events on the form
-function handleFormChange(event) {
-    if (checkIfAllFieldValueExists()) {
-        if ((event.target === letters || event.target === phone_calls) || (event.target === letter_calls_uplift_percentage && letter_calls_uplift_checkbox.checked)) {
-            updateDomElementTotal();
-        }
-        if (event.target === letter_calls_uplift_checkbox && !letter_calls_uplift_checkbox.checked) {
-            letter_calls_uplift_percentage.value = 0;
-            updateDomElementTotal();
-        }
-    }
-}
-
-// Attach event listener to the form for change events
+// Call initializeForm only if all necessary elements exist
 if (form && total_for_letters_and_phone_calls && letters && phone_calls) {
-    form.addEventListener('change', handleFormChange);
-}
+	initializeForm(
+		form,
+		total_for_letters_and_phone_calls,
+		letters,
+		phone_calls,
+		letter_calls_uplift_checkbox,
+		letter_calls_uplift_percentage
+	)
+};
