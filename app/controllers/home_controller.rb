1709840bb1389dcca9e689215faa07dce12d7601
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-06-16 16:20:10 +0100

    Add test error capture

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,14 @@
 class HomeController < ApplicationController
+  before_action :capture_test_error
   skip_before_action :authenticate_provider!
+  
+  #DELETE AFTER TESTING
+  def capture_test_error
+    begin
+      1 / 0
+    rescue ZeroDivisionError => exception
+      Sentry.capture_exception(exception)
+    end
+    
+    Sentry.capture_message("test message") 
 end
