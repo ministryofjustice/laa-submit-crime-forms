1cc6039e8ad5cdf2f246965378cfb0ebe1981b3a
mhunt3142 <128395220+mhunt3142@users.noreply.github.com>
2023-06-19 10:58:00 +0100

    claim_summary wip

--- app/controllers/steps/claim_summary_controller.rb
+++ app/controllers/steps/claim_summary_controller.rb
@@ -1,6 +1,8 @@
 module Steps
   class ClaimSummaryController < Steps::BaseStepController
     def edit
+      @claims={ preparation: 52.15, advocacy: 65.42, attendance_with_counsel: 35.68,
+        attendance_without_counsel: 35.68, travel: 27.6, waiting: 27.6}
       @work_items_by_date = current_application.work_items.group_by(&:completed_on)
       @form_object = AddAnotherForm.build(
         current_application
