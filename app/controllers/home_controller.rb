d28a405c633966472febd993b8d73b5321529328
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-06-21 16:37:34 +0100

    remove sentry test code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
   before_action :capture_test_error
   skip_before_action :authenticate_provider!
-
-  # DELETE AFTER TESTING
-  def capture_test_error
-    1 / 0
-  rescue ZeroDivisionError => e
-    Sentry.capture_exception(e)
-  end
 end
