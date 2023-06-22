4f6799e4a00cd0b279a5098aa78b725d4c6d4864
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-06-22 15:53:24 +0100

    Add pagination helper and handle page size params
    * PaginationHelper (code from Claim-For-Crown-Court-Defence) added to helpers to handle universal methods for page size and current page (with a method default_page_size to handle page size which can be changed per controller it is used in
    
    * page param is used to control which page the user is in
    
    *limit param is used to change limit of how many items per page

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,13 +1,12 @@
 class ClaimsController < ApplicationController
+  include PaginationHelpers
+  
   def index
     # TODO: delete old claims without a claim type or avoid creating
     # claim before we have a claim type - this breaks the pattern we
     # have used for the forms.
-    @claims = Claim.where(claim_type: ClaimType::SUPPORTED.map(&:to_s)).page(current_page)
-  end
-
-  def current_page
-    params[:page] ||  1
+    filtered_claims = Claim.where(claim_type: ClaimType::SUPPORTED.map(&:to_s))
+    @claims = filtered_claims.page(current_page).per(page_size)
   end
 
   def create
