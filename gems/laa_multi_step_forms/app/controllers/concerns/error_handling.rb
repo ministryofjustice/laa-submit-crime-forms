8ef5b0a12122bec1ee1fc55bccdef912bdd5e190
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-06-28 13:56:21 +0100

    fix incorrect usage of raise

--- gems/laa_multi_step_forms/app/controllers/concerns/error_handling.rb
+++ gems/laa_multi_step_forms/app/controllers/concerns/error_handling.rb
@@ -12,8 +12,6 @@ module ErrorHandling
       # when Errors::ApplicationSubmitted
       #   redirect_to application_submitted_errors_path
       else
-        raise
-
         Rails.logger.error(exception)
         Sentry.capture_exception(exception) if ENV.fetch('SENTRY_DSN', nil).present?
         redirect_to laa_msf.unhandled_errors_path if Rails.application.config.consider_all_requests_local
