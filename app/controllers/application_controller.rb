0dfef3c04fafb34e8ea588056dc4e5370be6a870
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-06-28 12:08:00 +0100

    Capture all Exceptions from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # TODO: use base controller from library??
 class ApplicationController < LaaMultiStepForms::ApplicationController
   include ApplicationHelper
+
+  rescue_from Exception, with: :unexpected_exception_handler
+
+  def unexpected_exception_handler(exception)
+    Sentry.capture_exception(exception)
+  end
 end
