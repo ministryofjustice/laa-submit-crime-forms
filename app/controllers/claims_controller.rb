b9a39122cf91c7d64882416c66366712cf7fa9c3
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-06-29 15:29:05 +0100

    Removed test code

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -7,8 +7,6 @@ class ClaimsController < ApplicationController
     # have used for the forms.
     filtered_claims = Claim.where(claim_type: ClaimType::SUPPORTED.map(&:to_s)).order('updated_at DESC')
     @claims = filtered_claims.page(current_page).per(page_size)
-    #  DELETE AFTER TESTING
-    Sentry.capture_message('Test')
   end
 
   def create
