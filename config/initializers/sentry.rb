628058058545e1b889519a8e879e878f9d4bc3c3
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-06-29 15:35:22 +0100

    Reduce sentry trace sample rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,7 +6,7 @@ if ENV.fetch('SENTRY_DSN', nil).present?
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = 1.0
+    config.traces_sample_rate = 0.05
     # or
     config.traces_sampler = lambda do |context|
       true
