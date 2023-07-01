31b0f9294b19beed960b22877cf9a6bd3813f974
mhunt3142 <128395220+mhunt3142@users.noreply.github.com>
2023-07-01 18:07:14 +0100

    CRM457-168-what-u-claim-update-the-what-are-you-claiming-for-page rubocop

--- spec/steps/claim_type/form_spec.rb
+++ spec/steps/claim_type/form_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe Steps::ClaimTypeForm do
   let(:cntp_date) { nil }
   let(:cntp_order) { nil }
 
-
   describe '#validations' do
     context 'when `ufn` is blank' do
       let(:ufn) { '' }
@@ -49,77 +48,76 @@ RSpec.describe Steps::ClaimTypeForm do
       end
     end
 
+    context 'when non-standard magistrate claim_type' do
+      let(:ufn) { 'UFN123' }
+      let(:claim_type) { ClaimType::NON_STANDARD_MAGISTRATE.to_s }
 
-      context 'when non-standard magistrate claim_type' do
-        let(:ufn) { 'UFN123' }
-        let(:claim_type) { ClaimType::NON_STANDARD_MAGISTRATE.to_s }
+      context 'with a rep order date' do
+        let(:rep_order_date) { Date.new(2023, 4, 1) }
+        let(:cntp_datee) { Date.new(2023, 4, 1) }
+        let(:cntp_order) { 'AAAA' }
 
-        context 'with a rep order date' do
-          let(:rep_order_date) { Date.new(2023, 4, 1) }
-          let(:cntp_datee) { Date.new(2023, 4, 1) }
-          let(:cntp_order) { 'AAAA' }
+        it { is_expected.to be_valid }
 
-          it { is_expected.to be_valid }
-
-          it 'can reset CNTP fields (leave rep order date)' do
-            attributes = subject.send(:attributes_to_reset)
-            expect(attributes).to eq(
-              'rep_order_date' => rep_order_date,
-              'cntp_order' => nil,
-              'cntp_date' => nil,
-            )
-          end
-        end
-
-        context 'without a rep order date' do
-          it 'is invalid' do
-            expect(subject).not_to be_valid
-            expect(subject.errors.of_kind?(:rep_order_date, :blank)).to be(true)
-          end
+        it 'can reset CNTP fields (leave rep order date)' do
+          attributes = subject.send(:attributes_to_reset)
+          expect(attributes).to eq(
+            'rep_order_date' => rep_order_date,
+            'cntp_order' => nil,
+            'cntp_date' => nil,
+          )
         end
       end
 
-      context 'when breach of injunction claim type' do
-        let(:ufn) { 'UFN123' }
-        let(:claim_type) { ClaimType::BREACH_OF_INJUNCTION.to_s }
+      context 'without a rep order date' do
+        it 'is invalid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.of_kind?(:rep_order_date, :blank)).to be(true)
+        end
+      end
+    end
 
-        context 'with a CNTP fields being set' do
-          let(:rep_order_date) { Date.new(2023, 4, 1) }
-          let(:cntp_date) { Date.new(2023, 4, 1) }
-          let(:cntp_order) { 'AAAA' }
+    context 'when breach of injunction claim type' do
+      let(:ufn) { 'UFN123' }
+      let(:claim_type) { ClaimType::BREACH_OF_INJUNCTION.to_s }
 
-          it 'is valid' do
-            expect(subject).to be_valid
-          end
+      context 'with a CNTP fields being set' do
+        let(:rep_order_date) { Date.new(2023, 4, 1) }
+        let(:cntp_date) { Date.new(2023, 4, 1) }
+        let(:cntp_order) { 'AAAA' }
 
-          it 'can reset rep order date (leave CNTP fields)' do
-            attributes = subject.send(:attributes_to_reset)
-            expect(attributes).to eq(
-              'rep_order_date' => nil,
-              'cntp_order' => cntp_order,
-              'cntp_date' => cntp_date,
-            )
-          end
+        it 'is valid' do
+          expect(subject).to be_valid
         end
 
-        context 'without a CNTP fields being set' do
-          it 'is also valid' do
-            expect(subject).not_to be_valid
-            expect(subject.errors.of_kind?(:cntp_order, :blank)).to be(true)
-            expect(subject.errors.of_kind?(:cntp_date, :blank)).to be(true)
-          end
+        it 'can reset rep order date (leave CNTP fields)' do
+          attributes = subject.send(:attributes_to_reset)
+          expect(attributes).to eq(
+            'rep_order_date' => nil,
+            'cntp_order' => cntp_order,
+            'cntp_date' => cntp_date,
+          )
         end
+      end
 
-        context 'without a CNTP date in the future being set' do
-          let(:cntp_date) { 3.days.from_now.to_date }
-          let(:cntp_order) { 'AAAA' }
+      context 'without a CNTP fields being set' do
+        it 'is also valid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.of_kind?(:cntp_order, :blank)).to be(true)
+          expect(subject.errors.of_kind?(:cntp_date, :blank)).to be(true)
+        end
+      end
 
-          it 'is also valid' do
-            expect(subject).not_to be_valid
-            expect(subject.errors.of_kind?(:cntp_order, :blank)).to be(false)
-            expect(subject.errors.of_kind?(:cntp_date, :future_not_allowed)).to be(true)
-          end
+      context 'without a CNTP date in the future being set' do
+        let(:cntp_date) { 3.days.from_now.to_date }
+        let(:cntp_order) { 'AAAA' }
+
+        it 'is also valid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.of_kind?(:cntp_order, :blank)).to be(false)
+          expect(subject.errors.of_kind?(:cntp_date, :future_not_allowed)).to be(true)
         end
       end
     end
   end
+end
