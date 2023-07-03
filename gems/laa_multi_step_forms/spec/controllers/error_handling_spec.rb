36f4327d5f4c8e104cb01361ee604f1e8e1a9941
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-03 10:52:44 +0100

    fix syntax issue on .to usage

--- gems/laa_multi_step_forms/spec/controllers/error_handling_spec.rb
+++ gems/laa_multi_step_forms/spec/controllers/error_handling_spec.rb
@@ -50,8 +50,8 @@ RSpec.describe DummyStepController, type: :controller do
 
     context 'non production RAILS_ENV' do
       before do
-        allow(ENV.to(receive(:[]))).and_call_original
-        allow(ENV.to(receive(:[]))).with('RAILS_ENV').and_return('development')
+        allow(ENV).to(receive(:[])).and_call_original
+        allow(ENV).to(receive(:[])).with('RAILS_ENV').and_return('development')
       end
 
       it 'raises the error' do
@@ -61,9 +61,9 @@ RSpec.describe DummyStepController, type: :controller do
 
     context 'production RAILS_ENV' do
       before do
-        allow(ENV.to(receive(:[]))).and_call_original
-        allow(ENV.to(receive(:[]))).with('RAILS_ENV').and_return('production')
-        allow(ENV.to(receive(:[]))).with('SENTRY_DSN').and_return('http://example.com')
+        allow(ENV).to(receive(:[])).and_call_original
+        allow(ENV).to(receive(:[])).with('RAILS_ENV').and_return('production')
+        allow(ENV).to(receive(:[])).with('SENTRY_DSN').and_return('http://example.com')
       end
 
       it 'logs the error' do
