ec47bf80bee331574bce0b38f6fca29ae9eb9bf1
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-06 17:08:46 +0100

    Cleanup
    Remove unused locale properties
    Remove unused migration

--- db/migrate/20230706142000_change_claim_details_strings_to_integers2.rb
+++ db/migrate/20230706142000_change_claim_details_strings_to_integers2.rb
@@ -1,4 +1,4 @@
-class ChangeClaimDetailsStringsToIntegers2 < ActiveRecord::Migration[7.0]
+class ChangeClaimDetailsStringsToIntegers < ActiveRecord::Migration[7.0]
   def change
     change_column :claims, :defence_statement, 'integer USING CAST(defence_statement AS integer)'
     change_column :claims, :prosecution_evidence, 'integer USING CAST(prosecution_evidence AS integer)'
