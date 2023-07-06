17926a13ff7007ba104b185eb054ceeda3cd5ebd
David Henry <david.henry@digital.justice.gov.uk>
2023-07-06 09:56:07 +0100

    Crm457 215/suggestion box (#116)
    ## Description of change
    Make autocomplete work as a suggestion box
    
    ## Link to relevant ticket
    https://dsdmoj.atlassian.net/browse/CRM457-215
    
    ## Notes to reviewer
    Read the Doc file first as this will hopefully explain how it works
    
    ## Additional note
    This would ideally have automated testing, but that requires integration
    test to use JS - which is outside the scope of this PR.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,9 +17,13 @@ import accessibleAutocomplete from 'accessible-autocomplete'
 const $acElements = document.querySelectorAll('[data-module="accessible-autocomplete"]')
 if ($acElements) {
   for (let i = 0; i < $acElements.length; i++) {
+    const name = $acElements[i].getAttribute('data-name')
+
     accessibleAutocomplete.enhanceSelectElement({
       selectElement: $acElements[i],
-      defaultValue: ''
+      defaultValue: '',
+      showNoOptionsFound: name === null,
+      name: name
     })
   }
 }
