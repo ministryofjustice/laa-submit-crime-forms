e6f17b615cd719b1d69539eab66e3d0aa045495d
David Henry <david.henry@digital.justice.gov.uk>
2023-07-10 10:02:16 +0100

    Crm457 209/disbursement summary (#119)
    ## Description of change
    Add Disbursement Summary details to the cost summary page
    
    ## Link to relevant ticket
    https://dsdmoj.atlassian.net/browse/CRM457-209

--- spec/factories/claims.rb
+++ spec/factories/claims.rb
@@ -2,5 +2,17 @@ FactoryBot.define do
   factory :claim do
     id { SecureRandom.uuid }
     office_code { 'AAA' }
+
+    trait :letters_calls do
+      rep_order_date { Date.yesterday }
+      letters { 2 }
+      calls { 3 }
+
+      navigation_stack { |claim| ["/applications/#{claim.id}/steps/letters_calls"] }
+
+      after(:build) do |claim, _context|
+        claim.navigation_stack << "/applications/#{claim.id}/steps/letters_calls"
+      end
+    end
   end
 end
