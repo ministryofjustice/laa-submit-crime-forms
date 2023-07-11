b03db1cf7f592d794ed78bd25e01a39772eeb1b8
Paul King <paul.king@digital.justice.gov.uk>
2023-07-11 09:42:40 +0100

    Remove auth for healthcheck

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HealthcheckController < ApplicationController
+  skip_before_action :authenticate_provider!
+  
   def ping
     render json: build_args, status: :ok
   end
