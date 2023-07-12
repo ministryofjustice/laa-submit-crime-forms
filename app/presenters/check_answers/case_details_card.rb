7eef131082750627ebbed465007ab4ad6c45ad4c
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-12 16:51:54 +0100

    Finish case details card with test

--- app/presenters/check_answers/case_details_card.rb
+++ app/presenters/check_answers/case_details_card.rb
@@ -1,8 +1,8 @@
 module CheckAnswers
   class CaseDetailsCard < Base
     attr_reader :case_details_form
-    
-    KEY = "case_details"
+
+    KEY = 'case_details'.freeze
 
     def initialize(claim)
       @case_details_form = Steps::CaseDetailsForm.build(claim)
@@ -16,32 +16,33 @@ module CheckAnswers
       I18n.t('steps.check_answers.groups.about_case.case_details.title')
     end
 
-    #TO DO: update remittal to include date of remittal when CRM457-172 is done
+    # TO DO: update remittal to include date of remittal when CRM457-172 is done
     def rows
       [
         {
           key: { text: translate_table_key(KEY, 'main_offence'), classes: 'govuk-summary-list__value-width-50' },
-          value: { text: case_details_form.main_offence },
+          value: { text: case_details_form.main_offence }
         },
         {
           key: { text: translate_table_key(KEY, 'main_offence_date'), classes: 'govuk-summary-list__value-width-50' },
-          value: { text: case_details_form.main_offence_date&.strftime('%d %B %Y') },
+          value: { text: case_details_form.main_offence_date&.strftime('%d %B %Y') }
         },
         {
           key: { text: translate_table_key(KEY, 'assigned_counsel'), classes: 'govuk-summary-list__value-width-50' },
-          value: { text: case_details_form.assigned_counsel.value.capitalize },
+          value: { text: capitalize_sym(case_details_form.assigned_counsel) }
         },
         {
           key: { text: translate_table_key(KEY, 'unassigned_counsel'), classes: 'govuk-summary-list__value-width-50' },
-          value: { text: case_details_form.unassigned_counsel.value.capitalize },
+          value: { text: capitalize_sym(case_details_form.unassigned_counsel) }
         },
         {
           key: { text: translate_table_key(KEY, 'agent_instructed'), classes: 'govuk-summary-list__value-width-50' },
-          value: { text: case_details_form.agent_instructed.value.capitalize },
+          value: { text: capitalize_sym(case_details_form.agent_instructed) }
         },
         {
-          key: { text: translate_table_key(KEY, 'remitted_to_magistrate'), classes: 'govuk-summary-list__value-width-50' },
-          value: { text: case_details_form.remitted_to_magistrate.value.capitalize },
+          key: { text: translate_table_key(KEY, 'remitted_to_magistrate'),
+classes: 'govuk-summary-list__value-width-50' },
+          value: { text: capitalize_sym(case_details_form.remitted_to_magistrate) }
         }
       ]
     end
