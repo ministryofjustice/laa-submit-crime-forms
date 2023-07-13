b64cdafaae0bc997300ad9d9ce10aaf944174c36
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-13 13:56:40 +0100

    Added details in defendant details section with tests

--- app/presenters/check_answers/defendant_details_card.rb
+++ app/presenters/check_answers/defendant_details_card.rb
@@ -1,17 +1,67 @@
 module CheckAnswers
   class DefendantDetailsCard < Base
-    attr_reader :defendant_details_form
+    attr_reader :defendant_details
+
+    KEY = 'defendant_summary'.freeze
 
     def initialize(claim)
-      # @defendant_details_form = Steps::DefendantDetailsForm.build(claim)
+      @defendant_details = claim.defendants
     end
 
     def route_path
-      'defendant_summary'
+      KEY
     end
 
     def title
       I18n.t('steps.check_answers.groups.about_defendant.defendant_details.title')
+    end
+
+    def rows
+      all_rows = []
+      all_rows.concat main_defendant_rows
+      additional_defendants.each_with_index do |defendant, index|
+        all_rows.concat additional_defendant_rows(defendant, index + 1)
+      end
+      all_rows
+    end
+
+    private
+
+    def main_defendant
+      defendant_details.find { |defendant| defendant[:main] == true }
+    end
+
+    def additional_defendants
+      defendant_details.select { |defendant| defendant[:main] == false }
+    end
+
+    def main_defendant_rows
+      [
+        {
+          key: { text: translate_table_key(KEY, 'main_defendant_full_name'),
+classes: 'govuk-summary-list__value-width-50' },
+          value: { text: main_defendant[:full_name] }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'main_defendant_maat'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: main_defendant[:maat] }
+        }
+      ]
+    end
+
+    def additional_defendant_rows(defendant, index)
+      [
+        {
+          key: { text: translate_table_key(KEY, 'additional_defendant_full_name', count: index),
+classes: 'govuk-summary-list__value-width-50' },
+          value: { text: defendant[:full_name] }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'additional_defendant_maat', count: index),
+classes: 'govuk-summary-list__value-width-50' },
+          value: { text: defendant[:maat] }
+        }
+      ]
     end
   end
 end
---
+++ spec/presenters/check_answers/defendant_details_card_spec.rb
@@ -0,0 +1,101 @@
+require 'rails_helper'
+
+RSpec.describe CheckAnswers::DefendantDetailsCard do
+  subject { described_class.new(claim) }
+
+  let(:claim) { instance_double(Claim) }
+  let(:defendants) { [] }
+  let(:main_defendant) { { main: true, full_name: main_defendant_name, maat: main_defendant_maat } }
+  let(:additional_defendant_1) do
+    { main: false, full_name: additional_defendant_1_name, maat: additional_defendant_1_maat }
+  end
+  let(:additional_defendant_2) do
+    { main: false, full_name: additional_defendant_2_name, maat: additional_defendant_2_maat }
+  end
+
+  let(:main_defendant_name) { 'Tim Roy' }
+  let(:main_defendant_maat) { '123ABC' }
+  let(:additional_defendant_1_name) { 'James Brown' }
+  let(:additional_defendant_1_maat) { '456EFG' }
+  let(:additional_defendant_2_name) { 'Timmy Turner' }
+  let(:additional_defendant_2_maat) { '789HIJ' }
+
+  before do
+    allow(claim).to receive(:defendants).and_return(defendants)
+  end
+
+  describe '#initialize' do
+    it 'creates the data instance' do
+      subject
+      expect(claim).to have_received(:defendants)
+    end
+  end
+
+  describe '#title' do
+    it 'shows correct title' do
+      expect(subject.title).to eq('Defendant details')
+    end
+  end
+
+  describe '#route_path' do
+    it 'is correct route' do
+      expect(subject.route_path).to eq('defendant_summary')
+    end
+  end
+
+  describe '#rows' do
+    context '1 main defendant and 2 additional defendants' do
+      let(:defendants) { [main_defendant, additional_defendant_1, additional_defendant_2] }
+
+      it 'generates full name and maat ref for 1 main defendant and 2 additional defendants' do
+        expect(subject.rows).to eq(
+          [
+            {
+              key: { text: 'Main defendant full name', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: 'Tim Roy' }
+            },
+            {
+              key: { text: 'Main defendant MAAT ID', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: '123ABC' }
+            },
+            {
+              key: { text: 'Additional defendant 1 full name', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: 'James Brown' }
+            },
+            {
+              key: { text: 'Additional defendant 1 MAAT ID', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: '456EFG' }
+            },
+            {
+              key: { text: 'Additional defendant 2 full name', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: 'Timmy Turner' }
+            },
+            {
+              key: { text: 'Additional defendant 2 MAAT ID', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: '789HIJ' }
+            },
+          ]
+        )
+      end
+    end
+
+    context '1 main defendant' do
+      let(:defendants) { [main_defendant] }
+
+      it 'generates full name and maat for 1 main defendant' do
+        expect(subject.rows).to eq(
+          [
+            {
+              key: { text: 'Main defendant full name', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: 'Tim Roy' }
+            },
+            {
+              key: { text: 'Main defendant MAAT ID', classes: 'govuk-summary-list__value-width-50' },
+              value: { text: '123ABC' }
+            }
+          ]
+        )
+      end
+    end
+  end
+end
