80191a790607f8eb7a56497d82037b89997fdc6e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-13 14:09:36 +0100

    Add hearing details rows and titles

--- app/presenters/check_answers/hearing_details_card.rb
+++ app/presenters/check_answers/hearing_details_card.rb
@@ -2,12 +2,47 @@ module CheckAnswers
   class HearingDetailsCard < Base
     attr_reader :hearing_details_form
 
+    KEY = 'hearing_details'.freeze
+
     def initialize(claim)
       @hearing_details_form = Steps::HearingDetailsForm.build(claim)
     end
 
     def route_path
-      'hearing_details'
+      KEY
+    end
+
+    def rows
+      [
+        {
+          key: { text: translate_table_key(KEY, 'hearing_date'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: 'Test' }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'no_of_hearings'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: 'Test' }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'proceedings_concluded'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: 'Test' }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'youth_court'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: 'Test' }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'mag_court'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: 'Test' }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'hearing_outcome'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: 'Test' }
+        },
+        {
+          key: { text: translate_table_key(KEY, 'matter_type'), classes: 'govuk-summary-list__value-width-50' },
+          value: { text: 'Test' }
+        }
+      ]
     end
 
     def title
