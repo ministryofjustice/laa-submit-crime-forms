a7c3384633f8b564577923041e33c00d5b00f8d4
David Henry <david.henry@digital.justice.gov.uk>
2023-07-14 14:24:28 +0100

    Enable duplicate button on work items

---
+++ gems/laa_multi_step_forms/app/lib/route_helpers.rb
@@ -0,0 +1,26 @@
+module RouteHelpers
+  def edit_step(name, opts = {}, &block)
+    resource name,
+             only: opts.fetch(:only, [:edit, :update]),
+             controller: name,
+             path_names: { edit: '' } do
+      yield if block
+    end
+  end
+
+  def crud_step(name, opts = {}, &block)
+    edit_step name, only: [] do
+      resources only: [:edit, :update, :destroy],
+                except: opts.fetch(:except, []),
+                controller: name, param: opts.fetch(:param),
+                path_names: { edit: '' } do
+        yield if block
+        get :confirm_destroy, on: :member if parent_resource.actions.include?(:destroy)
+      end
+    end
+  end
+
+  def show_step(name)
+    resource name, only: [:show], controller: name
+  end
+end
