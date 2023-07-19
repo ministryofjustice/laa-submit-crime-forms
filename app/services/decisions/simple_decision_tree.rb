5679afd97c6a02e43d3652696e9c12e2cf3fe0d3
David Henry <david.henry@digital.justice.gov.uk>
2023-07-19 14:09:34 +0100

    Some thoughts on how we could refactor the decision tree

--- app/services/decisions/simple_decision_tree.rb
+++ app/services/decisions/simple_decision_tree.rb
@@ -1,47 +1,95 @@
 module Decisions
   class SimpleDecisionTree < BaseDecisionTree
-    EDIT_MAPPING = {
-      defendant_details: :defendant_summary,
-      defendant_delete: :defendant_summary,
-      case_details: :hearing_details,
-      hearing_details: :case_disposal,
-      case_disposal: :reason_for_claim,
-      reason_for_claim: :claim_details,
-      claim_details: :work_item,
-      work_item: :work_items,
-      disbursement_cost: :disbursements,
-      other_info: :equality,
-    }.freeze
+    # should be a subclass of the Rule class, but allow additional methods to be added
+    RULE_CLASS = CustomRule
 
-    SHOW_MAPPING = {
-      solicitor_declaration: :claim_confirmation,
-    }.freeze
+    from(:defendant_details).goto(edit: :defendant_summary)
+    from(:defendant_details).goto(edit: :defendant_summary)
+    from(:defendant_delete).goto(edit: :defendant_summary)
+    from(:case_details).goto(edit: :hearing_details)
+    from(:hearing_details).goto(edit: :case_disposal)
+    from(:case_disposal).goto(edit: :reason_for_claim)
+    from(:reason_for_claim).goto(edit: :claim_details)
+    from(:claim_details).goto(edit: :work_item)
+    from(:work_item).goto(edit: :work_items)
+    from(:disbursement_cost).goto(edit: :disbursements)
+    from(:other_info).goto(edit: :equality)
+    from(:claim_type).goto(show: :start_page)
+    from(:solicitor_declaration).goto(show: :claim_confirmation)
+
+    from(:disbursement_cost)
+      .goto(edit: :disbursement_type, disbursement_type_id: -> { record.id })
+    from(:eqaulity)
+      .when(-> { answer_equality.yes? }).goto(edit: :equality_answers)
+      .goto(edit: :solicitor_declaration)
+    from(:defendant_summary)
+      .when(-> { add_another.yes? }).goto(edit: :defendant_details, defendant_id: StartPage::NEW_RECORD)
+      .when(-> { first_invalid(application.defendants, Steps::DefendantDetailsForm) }).goto(:defendant_details, defendant_id: ->(response) { response.id })
+      .goto(idit: :case_details)
+
+    class < self do
+      def rules
+        @rules ||= Hash.new { |k, h| h[k] = [] }
+      end
+
+      def from(source)
+        rules[source] << RULE_CLASS.new(from: source)
+      end
+    end
+
+    class Rule
+      def when(condition)
+        @condition = condition
+      end
+
+      def to(**destination)
+        @destinations ||= []
+        @destinations << [@condition, destination]
+        @condition = nil
+      end
+    end
+
+    class CustomRule < Rule
+      def first_invalid?(scope, *forms)
+        scope.detect { |record| forms.any? { |f| !f.build(record, application:).valid? } }
+      end
+    end
 
     def destination
-      if respond_to?("after_#{step_name}", true)
-        send("after_#{step_name}")
-      elsif EDIT_MAPPING[step_name]
-        edit(EDIT_MAPPING[step_name])
-      elsif SHOW_MAPPING[step_name]
-        show(SHOW_MAPPING[step_name])
+      rule = self.class.rules[step_name]
+      detected, destination = rule.detect do |condition, _|
+        condition.nil? || form_object.instance_eval(&condition)
+      end
+
+      destination.each_with_object({}) do |(key, value), hash|
+        hash[key] =
+          if value.responds_to(:call)
+            value.arity.zero ? form_object.instance_eval(&value) : value.call(detected)
+          else
+            value
+          end
+      end
+    end
+
+
+
+    def add_another(scope:, add_view:, sub_id:, form:, proceed_url:)
+      if form_object.add_another.yes?
+        edit(add_view, sub_id => StartPage::NEW_RECORD)
       else
-        index('/claims')
+        # we direct the user to any invalid forms when they choose next
+        forms = Array(form)
+        invalid_instance = scope.detect { |record| forms.any? { |f| !f.build(record, application:).valid? } }
+        if invalid_instance
+          edit(add_view, sub_id => invalid_instance.id, :flash => { error: 'Can not continue until valid!' })
+        else
+          proceed_url
+        end
       end
     end
 
     private
 
-    def after_disbursement_type
-      edit(:disbursement_cost, disbursement_id: form_object.record.id)
-    end
-
-    def after_equality
-      if form_object.answer_equality.yes?
-        show(:start_page, answer: 'yes')
-      else
-        edit(:solicitor_declaration)
-      end
-    end
 
     def after_defendant_summary
       add_another(
