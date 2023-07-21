50b69875dabd540d83bbf77089ac13f3bd934f6e
David Henry <david.henry@digital.justice.gov.uk>
2023-07-21 10:14:21 +0100

    rubocop

--- app/presenters/cost_summary/report.rb
+++ app/presenters/cost_summary/report.rb
@@ -27,7 +27,7 @@ module CostSummary
     end
 
     def total_cost
-      NumberTo.pounds(items.values.map(&:total_cost).compact.sum)
+      NumberTo.pounds(items.values.filter_map(&:total_cost).sum)
     end
 
     private
