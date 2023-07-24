da8eb53bcfa414b0d82aaab279f4a991ecf3bd53
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-24 09:55:10 +0100

    Add letters calls page section to check answers

--- app/presenters/check_answers/letters_calls_card.rb
+++ app/presenters/check_answers/letters_calls_card.rb
@@ -9,5 +9,18 @@ module CheckAnswers
       @group = 'about_claim'
       @section = 'letters_calls'
     end
+
+    def row_data
+      [
+        {
+          head_key: 'letters',
+          text: letters_calls_form.letters
+        },
+        {
+          head_key: 'calls',
+          text: letters_calls_form.calls
+        }
+      ]
+    end
   end
 end
