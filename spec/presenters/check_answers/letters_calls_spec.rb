1db67569d8e02e192353b97262e8ce6f26780749
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-24 10:09:18 +0100

    add test for letters and calls in check answers

---
+++ spec/presenters/check_answers/letters_calls_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe CheckAnswers::LettersCallsCard do
+  subject { described_class.new(claim) }
+
+  let(:claim) { instance_double(Claim) }
+  let(:letters_calls_form) do
+    instance_double(Steps::LettersCallsForm, letters:, calls:)
+  end
+
+  let(:letters) { 20 }
+  let(:calls) { 5 }
+
+  before do
+    allow(Steps::LettersCallsForm).to receive(:build).and_return(letters_calls_form)
+  end
+
+  describe '#initialize' do
+    it 'creates the data instance' do
+      subject
+      expect(Steps::LettersCallsForm).to have_received(:build).with(claim)
+    end
+  end
+
+  describe '#title' do
+    it 'shows correct title' do
+      expect(subject.title).to eq('Letters and phone calls')
+    end
+  end
+
+  describe '#route_path' do
+    it 'is correct route' do
+      expect(subject.route_path).to eq('letters_calls')
+    end
+  end
+
+  describe '#row_data' do
+    it 'generates hearing details rows' do
+      expect(subject.row_data).to eq(
+        [
+          {
+            head_key: 'letters',
+            text: 20
+          },
+          {
+            head_key: 'calls',
+            text: 5
+          }
+        ]
+      )
+    end
+  end
+end
