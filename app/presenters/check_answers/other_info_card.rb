05875d44ba497ba96dafa38d804f75efc119d1a9
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-25 15:52:05 +0100

    Add other info section information
    Ensured newlines get processed in text

--- app/presenters/check_answers/other_info_card.rb
+++ app/presenters/check_answers/other_info_card.rb
@@ -9,5 +9,20 @@ module CheckAnswers
       @group = 'about_claim'
       @section = 'other_info'
     end
+
+    def row_data
+      [
+        {
+          head_key: 'other_info', 
+          text: ActionController::Base.helpers.sanitize(formatted_info, tags: %w[br])
+        }
+      ]
+    end
+
+    private 
+
+    def formatted_info
+      other_info_form.other_info.gsub(/\n/, '<br>')
+    end
   end
 end
