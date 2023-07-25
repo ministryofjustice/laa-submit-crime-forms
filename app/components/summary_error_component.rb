173a241b44613b6dd5443b5134742956ab062dc2
David Henry <david.henry@digital.justice.gov.uk>
2023-07-25 14:04:14 +0100

    Add errors to the summary pages when invalid records

---
+++ app/components/summary_error_component.rb
@@ -0,0 +1,14 @@
+class SummaryErrorComponent < ViewComponent::Base
+  def initialize(records:, form:)
+    @form = form
+    @records = records
+  end
+
+  def forms
+    @forms ||= @records.map { |record| @form.build(record, application: helpers.current_application) }
+  end
+
+  def render?
+    ! forms.all?(&:valid?)
+  end
+end
