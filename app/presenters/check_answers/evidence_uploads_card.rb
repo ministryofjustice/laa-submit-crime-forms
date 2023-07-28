20d0592731834654ddf1b8c7daaeb2d4be74a35b
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-07-28 13:35:12 +0100

    Add populated section for supporting evidence

--- app/presenters/check_answers/evidence_uploads_card.rb
+++ app/presenters/check_answers/evidence_uploads_card.rb
@@ -2,9 +2,21 @@
 
 module CheckAnswers
   class EvidenceUploadsCard < Base
-    def initialize(_claim)
+    attr_reader :evidence_list
+    def initialize(claim)
       @group = 'supporting_evidence'
       @section = 'supporting_evidence'
+      @evidence_list = SupportingEvidence.where(claim_id: claim.id)
+    end
+
+    def row_data
+      evidence_list.map.with_index do |evidence, index|
+        {
+          head_key: 'supporting_evidence',
+          text: evidence.file_name,
+          head_opts: {count: index + 1}
+        }
+      end
     end
   end
 end
