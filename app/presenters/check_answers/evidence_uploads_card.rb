57b6498c724f936b452c42e2970fe2be5219d70b
Ivan Yohuno <ivan.yohuno@digital.justice.gov.uk>
2023-07-31 13:35:46 +0100

    Crm457 193 check answers claim justification/upload evidence (#187)
    ## Description of change
    Add Claim Justification section to Check Answers
    
    ## Link to relevant ticket
    [Check Answers Page](https://dsdmoj.atlassian.net/browse/CRM457-210)
    
    ## Notes for reviewer
    Best to be reviewed after [Check Answers Pt
    2](https://github.com/ministryofjustice/laa-claim-non-standard-magistrate-fee-backend/pull/180)
    
    ## Screenshots of changes (if applicable)
    <img width="713" alt="image"
    src="https://github.com/ministryofjustice/laa-claim-non-standard-magistrate-fee-backend/assets/26544538/4267bc4c-66a0-499e-8a84-6588283adc7f">

--- app/presenters/check_answers/evidence_uploads_card.rb
+++ app/presenters/check_answers/evidence_uploads_card.rb
@@ -2,9 +2,22 @@
 
 module CheckAnswers
   class EvidenceUploadsCard < Base
-    def initialize(_claim)
+    attr_reader :evidence_list
+
+    def initialize(claim)
       @group = 'supporting_evidence'
-      @section = 'firm_details'
+      @section = 'supporting_evidence'
+      @evidence_list = claim.supporting_evidence
+    end
+
+    def row_data
+      evidence_list.map.with_index do |evidence, index|
+        {
+          head_key: 'supporting_evidence',
+          text: evidence[:file_name],
+          head_opts: { count: index + 1 }
+        }
+      end
     end
   end
 end
---
+++ spec/presenters/check_answers/base_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe CheckAnswers::Base do
+  describe '#row_data' do
+    it 'generates empty placeholder array' do
+      expect(subject.row_data).to eq(
+        []
+      )
+    end
+  end
+end
