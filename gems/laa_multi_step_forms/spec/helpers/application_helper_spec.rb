36b41687d3f90b00aaf84eaa097bab6f27c6b00a
mhunt3142 <128395220+mhunt3142@users.noreply.github.com>
2023-07-31 12:29:12 +0100

    Crm457 253 update design of table in your claims page (#177)
    ## Description of change
    inclusion.of ufn and field re-ordering
    
    ## Link to relevant ticket
    https://dsdmoj.atlassian.net/browse/CRM457-253
    
    ## Notes for reviewer
    
    ## Screenshots of changes (if applicable)
    
    ### Before changes:
    
    ### After changes:
    
    ## How to manually test the feature
    
    ---------
    
    Co-authored-by: David Henry <david.henry@digital.justice.gov.uk>

--- gems/laa_multi_step_forms/spec/helpers/application_helper_spec.rb
+++ gems/laa_multi_step_forms/spec/helpers/application_helper_spec.rb
@@ -72,6 +72,31 @@ RSpec.describe LaaMultiStepForms::ApplicationHelper, type: :helper do
     end
   end
 
+  describe '#check_missing' do
+    context 'when value is false' do
+      it 'renders the missing tag' do
+        expect(helper.check_missing(false)).to eq('<strong class="govuk-tag govuk-tag--red">Incomplete</strong>')
+      end
+    end
+
+    context 'when value is truthy' do
+      context 'and no block is passed in' do
+        it 'renders the value' do
+          expect(helper.check_missing('apples')).to eq('apples')
+        end
+      end
+
+      context 'and ablock is passed in' do
+        it 'renders block' do
+          response = helper.check_missing('apples') do
+            'pears'
+          end
+          expect(response).to eq('pears')
+        end
+      end
+    end
+  end
+
   describe '#format_period' do
     context 'when period is nil' do
       it { expect(helper.format_period(nil)).to eq('*required') }
