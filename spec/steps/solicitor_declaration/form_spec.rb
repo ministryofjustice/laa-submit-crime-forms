568d591287491fab444ad7047240a4265a942c8f
David Henry <david.henry@digital.justice.gov.uk>
2023-08-02 16:18:14 +0100

    Add name validation throughout the application

--- spec/steps/solicitor_declaration/form_spec.rb
+++ spec/steps/solicitor_declaration/form_spec.rb
@@ -33,6 +33,14 @@ RSpec.describe Steps::SolicitorDeclarationForm do
         expect(form).to be_valid
       end
     end
+
+    ['Jim Bob', "J'm Bob", "Jim B'b", 'J-m Bob', 'Jim B-b', 'Jim Bob Bob', 'Jim B.b', 'Jim B,b'].each do |name|
+      context "it is a valid name string: #{name}" do
+        let(:signatory_name) { name }
+
+        it { expect(form).to be_valid }
+      end
+    end
   end
 
   describe 'form is #invalid' do
@@ -42,6 +50,17 @@ RSpec.describe Steps::SolicitorDeclarationForm do
       it 'is invalid' do
         expect(form).not_to be_valid
         expect(form.errors.of_kind?(:signatory_name, :blank)).to be(true)
+      end
+    end
+
+    ['Jim Bob!', 'Jim', 'Jim-Bob', 'jim@bob', 'jim the 1st'].each do |name|
+      context "it is a invalid name string: #{name}" do
+        let(:signatory_name) { name }
+
+        it 'is invalid' do
+          expect(form).not_to be_valid
+          expect(form.errors.of_kind?(:signatory_name, :invalid)).to be(true)
+        end
       end
     end
   end
