77476dc91e778d49cc864c97921748f66ca99360
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-08-10 17:28:36 +0100

    Add totals to disbursement card

--- app/presenters/check_answers/disbursement_costs_card.rb
+++ app/presenters/check_answers/disbursement_costs_card.rb
@@ -1,16 +1,52 @@
 # frozen_string_literal: true
 
 module CheckAnswers
-  class DisbursementCostsCard < CostSummary::Disbursements
-    attr_reader :section
-
-    def title(**_opt)
-      I18n.t('steps.check_answers.groups.about_claim.disbursements.title')
-    end
+  class DisbursementCostsCard < Base
+    attr_reader :claim, :disbursements
 
     def initialize(claim)
-      super(claim.disbursements, claim)
+      @claim = claim
+      @disbursements = CostSummary::Disbursements.new(claim.disbursements.by_age, claim)
       @section = 'disbursements'
+      @group = 'about_claim'
+    end
+
+    def row_data
+      header_rows + disbursement_rows + total_rows
+    end
+
+    private
+
+    def header_rows
+      [
+        {
+          head_key: 'items',
+          text: ApplicationController.helpers.sanitize(translate_table_key(section, 'items_total'), tags: %w[strong])
+        }
+      ]
+    end
+
+    def disbursement_rows
+      disbursements.rows.map do |row|
+        {
+          head_key: row[:key][:text],
+          text: row[:value][:text]
+        }
+      end
+    end
+
+    def total_rows
+      [
+        {
+          head_key: 'total', 
+          text: disbursement_total,
+          footer: true
+        }
+      ]
+    end
+
+    def disbursement_total
+      NumberTo.pounds(disbursements.total_cost)
     end
   end
 end
