62ffcfdbdd3566e700f8729b4cc3756ec658f612
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-08-10 15:07:57 +0100

    Add additional JEST test

--- spec/javascript/offence_autocomplete.spec.js
+++ spec/javascript/offence_autocomplete.spec.js
@@ -1,27 +1,36 @@
-import { suggestionTemplate } from "../../app/javascript/offence_autocomplete"
+import * as offenceAutocomplete from "../../app/javascript/offence_autocomplete"
 
-describe('suggestionTemplate', () => {
+describe("fetchOffences", () => {
 
-  test('valid result exists', () => {
+  test("api call returns nothing", async() => {
+    let offences = await offenceAutocomplete.fetchOffences()
+
+    expect(offences[0]).toEqual({name:"No entries found", type: ""})
+  })
+})
+
+describe("suggestionTemplate", () => {
+
+  test("valid result exists", () => {
     let result = {name: "Offence", type: "Offence Type"}
 
-    let suggestion = suggestionTemplate(result)
+    let suggestion = offenceAutocomplete.suggestionTemplate(result)
     
     expect(suggestion).toEqual('Offence<br><span class="autocomplete__caption">Offence Type</span>')
   })
 
-  test('invalid result exists', () => {
+  test("invalid result exists", () => {
     let result = {invalid_result: "true"}
 
-    let suggestion = suggestionTemplate(result)
+    let suggestion = offenceAutocomplete.suggestionTemplate(result)
     
     expect(suggestion).toEqual("")
   })
 
-  test('no result exists', () => {
+  test("no result exists", () => {
     let result = null
 
-    let suggestion = suggestionTemplate(result)
+    let suggestion = offenceAutocomplete.suggestionTemplate(result)
       
     expect(suggestion).toEqual("")
   })
