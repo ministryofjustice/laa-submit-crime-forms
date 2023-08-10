2dd019a34ebef04b93abdcca4385f183125a88a1
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-08-10 17:16:08 +0100

    Add total row to work items card in check answers

--- app/presenters/check_answers/work_items_card.rb
+++ app/presenters/check_answers/work_items_card.rb
@@ -22,12 +22,7 @@ module CheckAnswers
     end
 
     def row_data
-      header_rows + work_items.rows.map do |row|
-        {
-          head_key: row[:key][:text],
-          text: row[:value][:text]
-        }
-      end
+      header_rows + work_item_rows + total_rows
     end
 
     private
@@ -39,6 +34,29 @@ module CheckAnswers
           text: ApplicationController.helpers.sanitize(translate_table_key(section, 'items_total'), tags: %w[strong])
         }
       ]
+    end
+
+    def work_item_rows
+      work_items.rows.map do |row|
+        {
+          head_key: row[:key][:text],
+          text: row[:value][:text]
+        }
+      end 
+    end
+
+    def total_rows
+      [
+        {
+          head_key: 'total',
+          text: work_item_total,
+          footer: true
+        }
+      ]
+    end
+
+    def work_item_total
+      NumberTo.pounds(work_items.total_cost)
     end
   end
 end
