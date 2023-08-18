1f19e73e75069ef8d8c081f4f83e0977c36025e9
David Henry <david.henry@digital.justice.gov.uk>
2023-08-18 15:21:05 +0100

    remove commenting

--- app/presenters/check_answers/base.rb
+++ app/presenters/check_answers/base.rb
@@ -24,8 +24,10 @@ module CheckAnswers
     end
 
     def row_content(head_key, text, head_opts = {}, footer: false)
-      heading = translate_table_key(section, head_key, **head_opts)
-      # heading = translated_heading.start_with?('Translation missing:') ? head_key : translated_heading
+      translated_heading = translate_table_key(section, head_key, **head_opts)
+      # TODO: remove the below line once we understand why it was added as this is a smell
+      # as all keys should have a translation.
+      heading = translated_heading.start_with?('Translation missing:') ? head_key : translated_heading
       row = {
         key: {
           text: heading,
