0c02586848092b996419f37346cfc7351052b9a3
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-09-04 15:55:32 +0100

    Remove credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,6 @@
 if Rails.env.production?
   Aws.config.update({
                       region: ENV.fetch('AWS_REGION', 'eu-west-2'),
-                      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
                     })
 
   S3_BUCKET = Aws::S3::Resource.new.bucket(ENV.fetch('S3_BUCKET', 'default'))
