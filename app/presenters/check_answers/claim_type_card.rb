d333ca12f762a426487680f0d927d1b2c70c33eb
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-09-13 15:02:09 +0100

    Add base implementation

---
+++ app/presenters/check_answers/claim_type_card.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module CheckAnswers
+  class ClaimTypeCard < Base
+    attr_reader :claim
+
+    def initialize(claim)
+      @group = 'claim_type'
+      @section = 'claim_type'
+      @claim = claim
+    end
+
+    def row_data
+      claim_rows = is_magistrates_claim ? magistrates_claim : breach_claim
+
+      base_rows + claim_rows
+    end
+
+    def base_rows
+      [
+        {
+          head_key: 'file_ufn',
+          text: claim.ufn
+        },
+        {
+          head_key: 'type_of_claim',
+          text: translate_table_key(section, claim.claim_type)
+        }
+      ]
+    end
+
+    def magistrates_claim
+      [
+        {
+          head_key: 'rep_order_date',
+          text: claim.rep_order_date&.strftime('%d %B %Y')
+        }
+      ]
+    end
+
+    def breach_claim
+      [
+        {
+          head_key: 'cntp_number',
+          text: claim.cntp_order
+        },
+        {
+          head_key: 'cntp_rep_order',
+          text: claim.cntp_date&.strftime('%d %B %Y')
+        }
+      ]
+    end
+
+    def is_magistrates_claim
+      claim.claim_type == ClaimType::NON_STANDARD_MAGISTRATE.to_s
+    end
+  end
+end
+
