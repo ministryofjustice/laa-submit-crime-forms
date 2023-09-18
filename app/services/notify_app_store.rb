c1c48bcab0cb92c076ca24c13bcdc468bfa62227
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-09-18 22:04:41 +0100

    fix bad references

--- app/services/notify_app_store.rb
+++ app/services/notify_app_store.rb
@@ -15,7 +15,7 @@ class NotifyAppStore < ApplicationJob
     end
   end
 
-  def perform(claim, scorer: NotifyAppStore::Scorer)
+  def perform(claim, scorer: RiskAssessment::RiskAssessmentScorer)
     notify(MessageBuilder.new(claim:, scorer:))
   end
 
