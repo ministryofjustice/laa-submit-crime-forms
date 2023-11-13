8f59b8f7069b44f6fde66d3d7d6b6de640038b96
David Henry <david.henry@digital.justice.gov.uk>
2023-11-13 14:30:42 +0000

    Crm457 707/poll app store (#324)
    ## Description of change
    Add pooling functionality
    
    ## Link to relevant ticket
    [CRM457-707](https://dsdmoj.atlassian.net/browse/CRM457-707)
    
    ## Notes for reviewer
    This also adds the sidekiq web api within any security features which
    need to be fixed prior to go live
    
    [CRM457-707]:
    https://dsdmoj.atlassian.net/browse/CRM457-707?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ app/jobs/pull_updates.rb
@@ -0,0 +1,29 @@
+class PullUpdates < ApplicationJob
+  # queue :default
+
+  def perform
+    last_update = Claim.where.not(app_store_updated_at: nil)
+                       .maximum(:app_store_updated_at) || Time.zone.local(2023, 1, 1)
+
+    json_data = HttpPuller.new.get_all(last_update)
+
+    json_data['applications'].each do |record|
+      save(record['application_id'], convert_params(record))
+    end
+  end
+
+  private
+
+  def convert_params(record)
+    {
+      state: record['application_state'],
+      app_store_updated_at: record['updated_at'],
+    }
+  end
+
+  def save(claim_id, params)
+    claim = Claim.find_by(claim_id)
+
+    claim&.update!(params)
+  end
+end
