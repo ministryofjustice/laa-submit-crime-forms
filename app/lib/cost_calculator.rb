a1c874abf116a39d253d375571aa770ed7119fd4
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-12-20 12:01:09 +0000

    extract work item collection into it's own method

--- app/lib/cost_calculator.rb
+++ app/lib/cost_calculator.rb
@@ -12,7 +12,7 @@ module CostCalculator
     def travel_and_waiting_total(vat, object)
       pricing = Pricing.for(object)
       vat_rate = pricing.vat
-      total_without_vat = work_items_total(WorkItem.where(claim_id: object[:id], work_type: %w[travel waiting]), pricing)
+      total_without_vat = work_items_total(work_items(object), pricing)
       total = vat ? total_without_vat * (1 + vat_rate) : total_without_vat
       total.round(2)
     end
@@ -23,6 +23,10 @@ module CostCalculator
 
     def work_item_total(item, pricing)
       (item.time_spent / 60.to_f) * pricing[item.work_type] * (1 + (item[:uplift] / 100.to_f))
+    end
+
+    def work_items(object)
+      WorkItem.where(claim_id: object[:id], work_type: %w[travel waiting])
     end
   end
 end
