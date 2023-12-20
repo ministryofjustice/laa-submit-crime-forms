4ca1a33e84338c2117d5666fe660bb81216ae294
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-12-20 11:39:55 +0000

    amend method names

--- app/lib/cost_calculator.rb
+++ app/lib/cost_calculator.rb
@@ -12,16 +12,16 @@ module CostCalculator
     def travel_and_waiting_total(vat, object)
       pricing = Pricing.for(object)
       vat_rate = pricing.vat
-      total_without_vat = get_work_total(WorkItem.where(claim_id: object[:id], work_type: %w[travel waiting]), pricing)
+      total_without_vat = work_items_total(WorkItem.where(claim_id: object[:id], work_type: %w[travel waiting]), pricing)
       total = vat ? total_without_vat * (1 + vat_rate) : total_without_vat
       total.round(2)
     end
 
-    def get_work_total(items, pricing)
-      items.sum { |i| get_item_total(i, pricing) }
+    def work_items_total(items, pricing)
+      items.sum { |i| work_item_total(i, pricing) }
     end
 
-    def get_item_total(item, pricing)
+    def work_item_total(item, pricing)
       (item.time_spent / 60.to_f) * pricing[item.work_type] * (1 + (item[:uplift] / 100.to_f))
     end
   end
