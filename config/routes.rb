d8e2b80f618c3f73df581b2ecc53eb1b0eef8faa
David Henry <david.henry@digital.justice.gov.uk>
2024-01-10 10:13:10 +0000

    sidekiq auth in all environments

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_WEB_UI_USERNAME"])) &
       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_WEB_UI_PASSWORD"]))
-  end if Rails.env.production?
+  end
   mount Sidekiq::Web => "/sidekiq"
 
   extend RouteHelpers
