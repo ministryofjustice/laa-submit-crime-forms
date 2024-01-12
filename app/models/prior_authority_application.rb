48cedc3917b440621c22ec52a8afd663c9184cce
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-01-12 14:05:48 +0000

    add additional fields to prior auth model

--- app/models/prior_authority_application.rb
+++ app/models/prior_authority_application.rb
@@ -8,6 +8,9 @@ class PriorAuthorityApplication < ApplicationRecord
   attribute :firm_name, :string
   attribute :firm_account_number, :string
   attribute :ufn_form_status, :string
+  attribute :client_first_name, :string
+  attribute :client_last_name, :string
+  attribute :client_date_of_birth, :date
   attribute :case_contact_form_status, :string
   enum :status, { pre_draft: 'pre_draft', draft: 'draft', submitted: 'submitted' }
   attribute :created_at, :datetime
