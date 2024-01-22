d40579184b2ea425abf94bad4dc03916670f324a
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-01-22 13:19:41 +0000

    Add omniauth logging and silence test output
    with this this the omniauth logs will got to STDOOUT
    and, for testing, that means we polute the test output.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ require Rails.root.join('app/lib/laa_portal/saml_strategy')
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   configure do |config|
+    config.logger = Rails.logger
     config.logger.level = Logger::WARN if Rails.env.test?
     config.add_mock(:saml, LaaPortal::SamlStrategy.mock_auth)
     config.test_mode = Rails.env.test? || ENV.fetch('OMNIAUTH_TEST_MODE', 'false').inquiry.true?
