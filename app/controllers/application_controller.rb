ae70dd0a29552d1a7c5fc5a7efcdd90a36051c23
David Henry <david.henry@digital.justice.gov.uk>
2024-01-23 12:07:53 +0000

    Improve user auth to support multiple application types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < LaaMultiStepForms::ApplicationController
   include CookieConcern
 
   before_action :set_default_cookies
+  before_action :can_access_service
+
+  def cas_access_service
+    return if Provider::Gatekeeper.provider_enrolled?(service:)
+
+    Rails.logger.warn "Not enrolled provider access attempt, UID: #{auth_hash.uid}, " \
+                      "accounts: #{auth_hash.info.office_codes}, service: #{service}"
+
+    redirect_to laa_msf.not_enrolled_errors_path, flash: { notice: 'You do not have access to that srevice'}
+
+  end
+
+  def service
+    Provider::Gatekeeper::ANY
+  end
 end
--- app/services/providers/gatekeeper.rb
+++ app/services/providers/gatekeeper.rb
@@ -1,18 +1,24 @@
 # This class is used to limit User access
 module Providers
   class Gatekeeper
+    ANY_SERVICE = :any
+
     attr_reader :auth_info
 
     def initialize(auth_info)
       @auth_info = auth_info
     end
 
-    def provider_enrolled?
-      email_enrolled? || office_enrolled?
+    def provider_enrolled?(service: ANY_SERVICE)
+      email_enrolled? || office_enrolled?(service:)
     end
 
-    def office_enrolled?
-      allowed_office_codes.intersect?(auth_info.office_codes)
+    def office_enrolled?(service: ANY_SERVICE)
+      if service == ANY_SERVICE
+        auth_info.office_codes.any? { allowed_office_codes[_1] }
+      else
+        auth_info.office_codes.any? { allowed_office_codes[_1]&.include?(service.to_s) }
+      end
     end
 
     # TODO: implement separately once decided if this is required
