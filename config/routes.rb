9d6f8fb43ddad582ad558471a85ceb736aff9391
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-01-23 09:44:00 +0000

    Move files to new places

--- config/routes.rb
+++ config/routes.rb
@@ -48,66 +48,68 @@ Rails.application.routes.draw do
     get :accessibility
   end
 
-  resources :claims, except: [:edit, :show, :new, :update], as: :applications do
-    member do
-      get :delete
+  namespace :nsm, path: 'non-standard-magistrates' do
+    resources :claims, except: [:edit, :show, :new, :update], as: :applications do
+      member do
+        get :delete
+      end
     end
-  end
 
-  resources :offences, only: [:index], format: :js
-
-  namespace :steps do
-    namespace :office do
-      edit_step :confirm
-      edit_step :select
-    end
-  end
-
-
-  scope 'applications/:id' do
-    # This is used as a generic redirect once a draft has been commited
-    # The idea is that this can be custom to the implementation without
-    # requiring an additional method to store the path.
-    get '/steps/start_page', to: 'steps/start_page#show', as: 'after_commit'
+    resources :offences, only: [:index], format: :js
 
     namespace :steps do
-      edit_step :claim_type
-      show_step :start_page
-      edit_step :firm_details
-      edit_step :case_details
-      edit_step :case_disposal
-      edit_step :hearing_details
-      crud_step :defendant_details, param: :defendant_id, except: [:destroy]
-      edit_step :defendant_summary
-      crud_step :defendant_delete, param: :defendant_id, except: [:destroy]
-      edit_step :reason_for_claim
-      edit_step :claim_details
-      edit_step :letters_calls
-      crud_step :work_item, param: :work_item_id, except: [:destroy] do
-        member do
-          get :duplicate
+      namespace :office do
+        edit_step :confirm
+        edit_step :select
+      end
+    end
+
+
+    scope 'applications/:id' do
+      # This is used as a generic redirect once a draft has been commited
+      # The idea is that this can be custom to the implementation without
+      # requiring an additional method to store the path.
+      get '/steps/start_page', to: 'steps/start_page#show', as: 'after_commit'
+
+      namespace :steps do
+        edit_step :claim_type
+        show_step :start_page
+        edit_step :firm_details
+        edit_step :case_details
+        edit_step :case_disposal
+        edit_step :hearing_details
+        crud_step :defendant_details, param: :defendant_id, except: [:destroy]
+        edit_step :defendant_summary
+        crud_step :defendant_delete, param: :defendant_id, except: [:destroy]
+        edit_step :reason_for_claim
+        edit_step :claim_details
+        edit_step :letters_calls
+        crud_step :work_item, param: :work_item_id, except: [:destroy] do
+          member do
+            get :duplicate
+          end
         end
+        edit_step :work_items
+        crud_step :work_item_delete, param: :work_item_id, except: [:destroy]
+        edit_step :disbursement_add
+        crud_step :disbursement_type, param: :disbursement_id, except: [:destroy]
+        crud_step :disbursement_cost, param: :disbursement_id, except: [:destroy]
+        crud_step :disbursement_delete, param: :disbursement_id, except: [:destroy]
+        edit_step :disbursements
+        show_step :cost_summary
+        edit_step :other_info
+        upload_step :supporting_evidence do
+          collection {
+            get :download
+          }
+        end
+        edit_step :equality
+        edit_step :equality_questions
+        edit_step :solicitor_declaration
+        show_step :claim_confirmation
+        show_step :check_answers
+        show_step :view_claim
       end
-      edit_step :work_items
-      crud_step :work_item_delete, param: :work_item_id, except: [:destroy]
-      edit_step :disbursement_add
-      crud_step :disbursement_type, param: :disbursement_id, except: [:destroy]
-      crud_step :disbursement_cost, param: :disbursement_id, except: [:destroy]
-      crud_step :disbursement_delete, param: :disbursement_id, except: [:destroy]
-      edit_step :disbursements
-      show_step :cost_summary
-      edit_step :other_info
-      upload_step :supporting_evidence do
-        collection {
-          get :download
-        }
-      end
-      edit_step :equality
-      edit_step :equality_questions
-      edit_step :solicitor_declaration
-      show_step :claim_confirmation
-      show_step :check_answers
-      show_step :view_claim
     end
   end
 
