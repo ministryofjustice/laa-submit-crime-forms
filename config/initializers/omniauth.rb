412810d13e006e1ed50c4e7dde87fbdc39311187
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-01-23 15:57:58 +0000

    Revert "re-apply feature flags to app"
    This reverts commit b148f3075c49c1f92c9968f8ae91044cd78c749e.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     config.logger = Rails.logger
     config.logger.level = Logger::WARN if Rails.env.test?
     config.add_mock(:saml, LaaPortal::SamlStrategy.mock_auth)
-    config.test_mode = Rails.env.test? || FeatureFlags::FeatureFlag.active?(:omniauth_test_mode)
+    config.test_mode = Rails.env.test? || ENV.fetch('OMNIAUTH_TEST_MODE', 'false').inquiry.true?
 
     # This allow us to overwrite the fake auth settings and pretend to be different users for testing
     # ideally only the following keys should be set:
@@ -14,7 +14,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     # * info[name] => "Example User"
     # * info[email] => "provider@example.com"
     # * info[office_codes] => ["1A123B", "2A555X"]
-    if FeatureFlags::FeatureFlag.active?(:omniauth_test_mode)
+    if ENV.fetch('OMNIAUTH_TEST_MODE', 'false').inquiry.true?
       config.before_callback_phase do |env|
         env['omniauth.auth'].merge!(
           Rack::Utils.parse_nested_query(env['QUERY_STRING'])
