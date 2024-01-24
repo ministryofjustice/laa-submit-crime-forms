3fadda6f74ac491cf423e2efd8ed72c8d578e758
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-01-24 13:32:03 +0000

    use to_prepare to run commands arfter initializers are run for Railties

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,19 +6,21 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     config.logger = Rails.logger
     config.logger.level = Logger::WARN if Rails.env.test?
     config.add_mock(:saml, LaaPortal::SamlStrategy.mock_auth)
-    config.test_mode = Rails.env.test? || FeatureFlags::FeatureFlag.active?(:omniauth_test_mode)
+    ActiveSupport::Reloader.to_prepare do
+      config.test_mode = Rails.env.test? || FeatureFlags::FeatureFlag.active?(:omniauth_test_mode)
 
-    # This allow us to overwrite the fake auth settings and pretend to be different users for testing
-    # ideally only the following keys should be set:
-    # * uid => "test-user"
-    # * info[name] => "Example User"
-    # * info[email] => "provider@example.com"
-    # * info[office_codes] => ["1A123B", "2A555X"]
-    if FeatureFlags::FeatureFlag.active?(:omniauth_test_mode)
-      config.before_callback_phase do |env|
-        env['omniauth.auth'].merge!(
-          Rack::Utils.parse_nested_query(env['QUERY_STRING'])
-        )
+      # This allow us to overwrite the fake auth settings and pretend to be different users for testing
+      # ideally only the following keys should be set:
+      # * uid => "test-user"
+      # * info[name] => "Example User"
+      # * info[email] => "provider@example.com"
+      # * info[office_codes] => ["1A123B", "2A555X"]
+      if FeatureFlags::FeatureFlag.active?(:omniauth_test_mode)
+        config.before_callback_phase do |env|
+          env['omniauth.auth'].merge!(
+            Rack::Utils.parse_nested_query(env['QUERY_STRING'])
+          )
+        end
       end
     end
   end
