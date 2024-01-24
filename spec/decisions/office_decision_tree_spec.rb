15d5bf0d5cf8b4be0dc1d905eabb7dd1fd1e02f4
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-01-24 14:26:18 +0000

    Fix office decision tree bug

--- spec/decisions/office_decision_tree_spec.rb
+++ spec/decisions/office_decision_tree_spec.rb
@@ -1,20 +1,22 @@
+require 'rails_helper'
+
 RSpec.describe Decisions::OfficeDecisionTree do
   let(:id) { SecureRandom.uuid }
   let(:application) { build(:provider) }
   let(:record) { application }
   let(:form) { double(:form, application:, record:) }
 
-  it_behaves_like 'a generic decision', from: :select_office, goto: { action: :index, controller: '/claims' }
+  it_behaves_like 'a generic decision', from: :select_office, goto: { action: :index, controller: '/nsm/claims' }
 
   context 'answer yes to is_current_office' do
     before { allow(form).to receive(:is_current_office).and_return(YesNoAnswer::YES) }
 
-    it_behaves_like 'a generic decision', from: :confirm_office, goto: { action: :index, controller: '/claims' }
+    it_behaves_like 'a generic decision', from: :confirm_office, goto: { action: :index, controller: '/nsm/claims' }
   end
 
   context 'answer no to is_current_office' do
     before { allow(form).to receive(:is_current_office).and_return(YesNoAnswer::NO) }
 
-    it_behaves_like 'a generic decision', from: :confirm_office, goto: { action: :edit, controller: 'steps/office/select' }
+    it_behaves_like 'a generic decision', from: :confirm_office, goto: { action: :edit, controller: '/nsm/steps/office/select' }
   end
 end
