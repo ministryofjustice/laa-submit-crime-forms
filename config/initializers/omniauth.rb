3cf2ee578665df84baaf4bd1208bf921c7dcff66
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-01-24 11:46:36 +0000

    Remove additional usage of FeatureFlag in initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     config.logger = Rails.logger
     config.logger.level = Logger::WARN if Rails.env.test?
     config.add_mock(:saml, LaaPortal::SamlStrategy.mock_auth)
-    config.test_mode = Rails.env.test? || FeatureFlags::FeatureFlag.active?(:omniauth_test_mode)
+    config.test_mode = Rails.env.test? || ENV.fetch('OMNIAUTH_TEST_MODE', 'false').inquiry.true?
 
     # This allow us to overwrite the fake auth settings and pretend to be different users for testing
     # ideally only the following keys should be set:
