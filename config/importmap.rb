c87c4d19c2c69d9b65f557b387ecef1a243084c6
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-01-25 16:23:39 +0000

    BUG: MOJ frontend js fail (#466)
    ## Description of change
    Somehow I didn't notice I broke the importmap for MOJ 2.0.
    Also while testing I came across some corrupt I18n keys that, in all
    likelihood, I also broke when re-namespacing

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,5 +9,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "govuk-frontend", to: "https://ga.jspm.io/npm:govuk-frontend@5.0.0/dist/govuk/all.mjs"
 pin "accessible-autocomplete", to: "https://ga.jspm.io/npm:accessible-autocomplete@2.0.4/dist/accessible-autocomplete.min.js"
-pin "@ministryofjustice/frontend", to: "@ministryofjustice--frontend.js" # @2.0.0
+pin "@ministryofjustice/frontend", to: "https://ga.jspm.io/npm:@ministryofjustice/frontend@2.0.0/moj/all.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
