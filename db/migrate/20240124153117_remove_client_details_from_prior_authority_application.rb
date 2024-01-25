f41911f8827d763ddd9caf38e35d8fb28db0657b
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-01-25 12:00:30 +0000

    Remove problematic attempted migration of test data

--- db/migrate/20240124153117_remove_client_details_from_prior_authority_application.rb
+++ db/migrate/20240124153117_remove_client_details_from_prior_authority_application.rb
@@ -1,15 +1,5 @@
 class RemoveClientDetailsFromPriorAuthorityApplication < ActiveRecord::Migration[7.1]
   def change
-    PriorAuthorityApplication.find_each do |application|
-      Defendant.create!(
-        defendable_id: application.id,
-        defendable_type: 'PriorAuthorityApplication',
-        first_name: application.client_first_name,
-        last_name: application.client_last_name,
-        date_of_birth: application.client_date_of_birth,
-      )
-    end
-
     change_table :prior_authority_applications, bulk: true do |t|
       t.remove :client_date_of_birth
       t.remove :client_first_name
