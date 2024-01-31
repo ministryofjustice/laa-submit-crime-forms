d3e2c84cc8754051bf60efb9ba99e2e87f708e6d
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-01-31 16:59:56 +0000

    simplify and fix code for can_start?

--- app/presenters/prior_authority/tasks/primary_quote.rb
+++ app/presenters/prior_authority/tasks/primary_quote.rb
@@ -1,7 +1,6 @@
 module PriorAuthority
   module Tasks
     class PrimaryQuote < ::Tasks::Generic
-      PREVIOUS_TASK = CaseAndHearingDetail
       FORM = ::PriorAuthority::Steps::PrimaryQuoteForm
 
       def path
@@ -9,30 +8,11 @@ module PriorAuthority
       end
 
       def can_start?
-        if application.prison_law === true
-          fulfilled?(CaseAndHearingDetail)
-        else
-          if application.court_type == PriorAuthority::CourtTypeOptions::MAGISTRATE.to_s
-            fulfilled?(CaseAndHearingDetail) && youth_court_form_filled?
-          elsif application.court_type == PriorAuthority::CourtTypeOptions::CENTRAL_CRIMINAL.to_s
-            fulfilled?(CaseAndHearingDetail) && psychiatric_liaison_form_filled?
-          end
-        end
+        fulfilled?(CaseAndHearingDetail) && fulfilled?(ClientDetail)
       end
 
       def record
         application.primary_quote
-      end
-
-      private
-
-      def youth_court_form_filled?
-        application.youth_court.present?
-      end
-
-      def psychiatric_liaison_form_filled?
-        Rails.logger.debug application.psychiatric_liaison_reason_not.present?
-        application.psychiatric_liaison == true ? application.psychiatric_liaison : application.psychiatric_liaison_reason_not.present?
       end
     end
   end
