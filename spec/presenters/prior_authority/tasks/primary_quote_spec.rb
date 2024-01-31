4ae241b8292cd6a5fd5d3536c53bb4fe3c22370a
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-01-31 10:42:15 +0000

    fix more tests

--- spec/presenters/prior_authority/tasks/primary_quote_spec.rb
+++ spec/presenters/prior_authority/tasks/primary_quote_spec.rb
@@ -10,18 +10,4 @@ RSpec.describe PriorAuthority::Tasks::PrimaryQuote, type: :presenter do
 
     it { is_expected.to eq("/prior-authority/applications/#{application.id}/steps/primary_quote") }
   end
-
-  describe '#completed?' do
-    context 'when primary quote is completed' do
-      before do
-        create(:quote, primary: true, prior_authority_application: application)
-      end
-
-      it { is_expected.to be_completed }
-    end
-
-    context 'when primary quote is not completed' do
-      it { is_expected.not_to be_completed }
-    end
-  end
 end
