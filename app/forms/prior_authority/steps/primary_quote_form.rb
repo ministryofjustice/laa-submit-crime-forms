c0f329e0c795b0aaf5c7b19ab573014592bca51e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-05 14:25:40 +0000

    Code to account for users adding non-custom value but not selecting from dropdown

--- app/forms/prior_authority/steps/primary_quote_form.rb
+++ app/forms/prior_authority/steps/primary_quote_form.rb
@@ -13,8 +13,8 @@ module PriorAuthority
       validates :postcode, presence: true, uk_postcode: true
 
       def service_type_suggestion=(value)
-        if value.in?(translated_values)
-          self.service_type = service_type.value
+        if value.downcase.in?(translated_values)
+          self.service_type = service_type_code(value)
           self.custom_service_name = nil
         else
           self.service_type = 'custom' if value.present?
@@ -35,7 +35,11 @@ module PriorAuthority
       end
 
       def translated_values
-        QuoteServices.values.map(&:translated)
+        QuoteServices.values.map{|value| value.translated.downcase }
+      end
+
+      def service_type_code(service_type_string)
+       I18n.t("helpers.quote_services").transform_values(&:downcase).key(service_type_string.downcase)
       end
     end
   end
