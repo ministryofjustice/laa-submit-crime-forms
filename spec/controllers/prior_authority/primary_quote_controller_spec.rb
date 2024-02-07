289942e759667b7b3174c2debc9ef0fe0f1395cc
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-07 15:41:07 +0000

    add some controller tests for file upload

--- spec/controllers/prior_authority/primary_quote_controller_spec.rb
+++ spec/controllers/prior_authority/primary_quote_controller_spec.rb
@@ -8,6 +8,47 @@ RSpec.describe PriorAuthority::Steps::PrimaryQuoteController, type: :controller 
     allow(controller).to receive(:current_application).and_return(application)
   end
 
+  describe '#update' do
+    context 'when a file is uploaded' do
+      before do
+        request.env['CONTENT_TYPE'] = 'image/png'
+        expect(Clamby).to receive(:safe?).and_return(true)
+        put :update, params: { id: '12345', prior_authority_steps_primary_quote_form: { documents: fixture_file_upload('test.png', 'image/png') } }
+      end
+
+      it 'returns a successful response' do
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when no file is uploaded' do
+      before do
+        request.env['CONTENT_TYPE'] = 'image/png'
+        put :update, params: { id: '12345', prior_authority_steps_primary_quote_form: { documents: nil }}
+      end
+
+      it 'returns a successful response' do
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when vulnerable file type is uploaded' do
+      before do
+        request.env['CONTENT_TYPE'] = 'image/png'
+        expect(Clamby).to receive(:safe?).and_return(false)
+        put :update, params: { id: '12345', prior_authority_steps_primary_quote_form: { documents: fixture_file_upload('test.png', 'image/png') }}
+      end
+
+      it 'redirects back to form' do
+        expect(response).to redirect_to(edit_prior_authority_steps_primary_quote_path(application))
+      end
+
+      it 'generates flash error' do
+        expect(flash[:alert]).to eq('File potentially contains malware so cannot be uploaded. Please contact your administrator')
+      end
+    end
+  end
+
   describe '#primary_quote' do
     context 'non custom primary quote service' do
       it 'returns a quote with non custom service type' do
