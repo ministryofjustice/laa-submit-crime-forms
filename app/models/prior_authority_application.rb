994f7b27666e2dc9ad8fbd71d209eed27922f5be
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-08 11:44:04 +0000

    fix rubocop issue

--- app/models/prior_authority_application.rb
+++ app/models/prior_authority_application.rb
@@ -15,10 +15,10 @@ class PriorAuthorityApplication < ApplicationRecord
   has_one :primary_quote_document, lambda {
                                      where(document_type: SupportingDocument::PRIMARY_QUOTE_DOCUMENT)
                                    },
-           dependent: :destroy,
-           inverse_of: :documentable,
-           class_name: 'SupportingDocument',
-           as: :documentable
+          dependent: :destroy,
+          inverse_of: :documentable,
+          class_name: 'SupportingDocument',
+          as: :documentable
   has_many :additional_costs, dependent: :destroy
 
   attribute :prison_law, :boolean
