531dfd32c1b5cea91e6496162a953e8ab318ff04
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-08 17:29:27 +0000

    Revert "update factories to include quotes with documents"
    This reverts commit 672cc814ef5c5366ffa7517bb1e5c9bc22dd9b48.

--- spec/factories/supporting_documents.rb
+++ spec/factories/supporting_documents.rb
@@ -13,9 +13,5 @@ FactoryBot.define do
       documentable_type { 'Claim' }
       document_type { 'supporting_evidence' }
     end
-
-    factory :quote_document do
-      document_type { 'quote_document' }
-    end
   end
 end
