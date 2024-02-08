98011f468f19d75b3c3d49693908bcfd506db5ba
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-08 17:30:09 +0000

    fix rubocop issues

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,8 +1,8 @@
 class Quote < ApplicationRecord
   belongs_to :prior_authority_application
   has_one :document, lambda {
-                                     where(document_type: SupportingDocument::QUOTE_DOCUMENT)
-                                   },
+                       where(document_type: SupportingDocument::QUOTE_DOCUMENT)
+                     },
           dependent: :destroy,
           inverse_of: :documentable,
           class_name: 'SupportingDocument',
