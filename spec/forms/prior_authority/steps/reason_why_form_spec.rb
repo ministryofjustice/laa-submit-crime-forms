44232b3dc19d68cdb802517f2260775ab8a11833
David Henry <david.henry@digital.justice.gov.uk>
2024-02-12 11:10:01 +0000

    Add missing error condition

--- spec/forms/prior_authority/steps/reason_why_form_spec.rb
+++ spec/forms/prior_authority/steps/reason_why_form_spec.rb
@@ -28,6 +28,24 @@ RSpec.describe PriorAuthority::Steps::ReasonWhyForm do
         expect(form.errors.messages[:reason_why]).to include('Enter why prior authority is required')
       end
     end
+
+    context 'when reason_why great than 2000 characters' do
+      let(:reason_why) { 'apples ' * 286 } # 7 * 286 = 2002
+
+      it 'has a validation error on the field' do
+        expect(form).not_to be_valid
+        expect(form.errors.of_kind?(:reason_why, :too_long)).to be(true)
+        expect(form.errors.messages[:reason_why]).to include(
+          'The reaosn why you need prior authority cannot be more than 2000 characters.'
+        )
+      end
+    end
+
+    context 'when reason_why has non-ascii characters but the byte length is > 2000' do
+      let(:reason_why) { "\u2714 " * 800 } # 2 * 800 = 1600 char, 3 * 800 = 2400 bytes
+
+      it { expect(form).to be_valid }
+    end
   end
 
   describe '#save' do
