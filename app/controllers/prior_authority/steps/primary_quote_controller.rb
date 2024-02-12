1e155d093b3c302fc333293a768bf819d21e3645
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-12 18:15:31 +0000

    fix logic for missing file upload if saving draft

--- app/controllers/prior_authority/steps/primary_quote_controller.rb
+++ app/controllers/prior_authority/steps/primary_quote_controller.rb
@@ -13,9 +13,11 @@ module PriorAuthority
       def update
         record = primary_quote
         @primary_quote_document = current_application.primary_quote.document
-        return if file_error_present
+        pending_document = params[:prior_authority_steps_primary_quote_form][:document]
 
-        upload_file(params)
+        return if file_error_present(pending_document)
+
+        upload_file(pending_document) unless pending_document.nil?
         update_and_advance(PrimaryQuoteForm, as:, after_commit_redirect_path:, record:)
       rescue FileUpload::FileUploader::PotentialMalwareError => e
         return_error({ message: t('shared.shared_upload_errors.malware') }, e)
@@ -43,9 +45,9 @@ module PriorAuthority
         @file_uploader ||= FileUpload::FileUploader.new
       end
 
-      def upload_file(params)
-        file_path = file_uploader.upload(params[:prior_authority_steps_primary_quote_form][:document])
-        save_file(params[:prior_authority_steps_primary_quote_form][:document], file_path)
+      def upload_file(file)
+        file_path = file_uploader.upload(file)
+        save_file(file, file_path)
       end
 
       def save_file(params, file_path)
@@ -58,23 +60,23 @@ module PriorAuthority
         record.save!
       end
 
-      def supported_filetype
+      def supported_filetype(file)
         SupportedFileTypes::QUOTE_DOCUMENT
-          .include? params[:prior_authority_steps_primary_quote_form][:document].content_type
+          .include? file.content_type
       end
 
-      def file_size_within_limit
-        params[:prior_authority_steps_primary_quote_form][:document].tempfile.size <= ENV.fetch(
+      def file_size_within_limit(file)
+        file.tempfile.size <= ENV.fetch(
           'MAX_UPLOAD_SIZE_BYTES', nil
         ).to_i
       end
 
-      def file_error_present
-        if params[:prior_authority_steps_primary_quote_form][:document].nil?
+      def file_error_present(file)
+        if file.nil? && !params[:commit_draft]
           return_error({ message: t('shared.shared_upload_errors.file_not_present', file: 'primary quote') })
-        elsif !file_size_within_limit
+        elsif file && !file_size_within_limit(file)
           return_error({ message: t('shared.shared_upload_errors.file_size_limit', max_size: '10MB') })
-        elsif !supported_filetype
+        elsif file && !supported_filetype(file)
           return_error({ message: t('shared.shared_upload_errors.file_type',
                                     file_types: t('shared.shared_upload_errors.file_types')) })
         else
@@ -89,8 +91,7 @@ module PriorAuthority
           Sentry.capture_exception(exception)
         end
         flash[:alert] = dict[:message]
-        record = primary_quote
-        update_and_advance(PrimaryQuoteForm, as:, after_commit_redirect_path:edit_prior_authority_steps_primary_quote_path(current_application), record:)
+        redirect_to edit_prior_authority_steps_primary_quote_path(current_application)
       end
     end
   end
