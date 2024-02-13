daeac115c1c150a532401ad4d71093b9ee1e006a
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-13 09:38:22 +0000

    add ability to upload file and error handling to primary quote controller

--- app/controllers/prior_authority/steps/primary_quote_controller.rb
+++ app/controllers/prior_authority/steps/primary_quote_controller.rb
@@ -6,11 +6,23 @@ module PriorAuthority
           primary_quote,
           application: current_application
         )
+
+        @primary_quote_document = current_application.primary_quote.document
       end
 
       def update
         record = primary_quote
+        @primary_quote_document = current_application.primary_quote.document
+        pending_document = params[:prior_authority_steps_primary_quote_form][:document]
+
+        return if file_error_present(pending_document)
+
+        upload_file(pending_document) unless pending_document.nil?
         update_and_advance(PrimaryQuoteForm, as:, after_commit_redirect_path:, record:)
+      rescue FileUpload::FileUploader::PotentialMalwareError => e
+        return_error({ message: t('shared.shared_upload_errors.malware') }, e)
+      rescue StandardError => e
+        return_error({ message: t('shared.shared_upload_errors.unable_upload') }, e)
       end
 
       private
@@ -27,6 +39,59 @@ module PriorAuthority
 
       def additional_permitted_params
         [:service_type_suggestion]
+      end
+
+      def file_uploader
+        @file_uploader ||= FileUpload::FileUploader.new
+      end
+
+      def upload_file(file)
+        file_path = file_uploader.upload(file)
+        save_file(file, file_path)
+      end
+
+      def save_file(params, file_path)
+        record = current_application.primary_quote.document || current_application.primary_quote.build_document
+        record.document_type = SupportingDocument::QUOTE_DOCUMENT
+        record.file_name = params.original_filename
+        record.file_type = params.content_type
+        record.file_size = params.tempfile.size
+        record.file_path = file_path
+        record.save!
+      end
+
+      def supported_filetype(file)
+        SupportedFileTypes::QUOTE_DOCUMENT
+          .include? file.content_type
+      end
+
+      def file_size_within_limit(file)
+        file.tempfile.size <= ENV.fetch(
+          'MAX_UPLOAD_SIZE_BYTES', nil
+        ).to_i
+      end
+
+      def file_error_present(file)
+        if file.nil? && !params[:commit_draft]
+          return_error({ message: t('shared.shared_upload_errors.file_not_present', file: 'primary quote') })
+        elsif file && !file_size_within_limit(file)
+          return_error({ message: t('shared.shared_upload_errors.file_size_limit', max_size: '10MB') })
+        elsif file && !supported_filetype(file)
+          return_error({ message: t('shared.shared_upload_errors.file_type',
+                                    file_types: t('shared.shared_upload_errors.file_types')) })
+        else
+          false
+        end
+      end
+
+      def return_error(dict, exception = nil)
+        if exception.nil?
+          Sentry.capture_exception(dict[:message])
+        else
+          Sentry.capture_exception(exception)
+        end
+        flash[:alert] = dict[:message]
+        redirect_to edit_prior_authority_steps_primary_quote_path(current_application)
       end
     end
   end
