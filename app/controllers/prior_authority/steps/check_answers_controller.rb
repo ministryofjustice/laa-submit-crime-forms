f9d8ae016cd82df921d895e9cb3ffb45045f62fb
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-02-16 09:37:40 +0000

    CRM457-1132: Add confirmations to check your answers

--- app/controllers/prior_authority/steps/check_answers_controller.rb
+++ app/controllers/prior_authority/steps/check_answers_controller.rb
@@ -6,19 +6,30 @@ module PriorAuthority
       #
       # before_action :check_complete?
 
+      before_action :build_report
+
       def edit
-        @application = current_application
         @application.update!(navigation_stack: stack_with_step_moved_to_end)
 
-        @report = CheckAnswers::Report.new(@application)
+        @form_object = CheckAnswersForm.build(
+          current_application
+        )
       end
 
-      # TODO: CRM457-1132 check answers confirmation form
       def update
-        # update_and_advance(CheckAnswersForm, as:, after_commit_redirect_path:)
+        update_and_advance(CheckAnswersForm, as:, after_commit_redirect_path:)
       end
 
       private
+
+      def as
+        :check_answers
+      end
+
+      def build_report
+        @application = current_application
+        @report = CheckAnswers::Report.new(@application)
+      end
 
       def stack_with_step_moved_to_end
         stack_with_step_moved_to_end = @application.navigation_stack.delete_if { |step| step == request.fullpath }
---
+++ app/forms/prior_authority/steps/check_answers_form.rb
@@ -0,0 +1,22 @@
+module PriorAuthority
+  module Steps
+    class CheckAnswersForm < ::Steps::BaseFormObject
+      attribute :confirm_excluding_vat, :boolean
+      attribute :confirm_travel_expenditure, :boolean
+
+      validates :confirm_excluding_vat, acceptance: { accept: true }, allow_nil: false
+      validates :confirm_travel_expenditure, acceptance: { accept: true }, allow_nil: false
+
+      def persist!
+        application.update!(attributes)
+
+        # TODO: placeholder for possible logic to actually submit
+        # PriorAuthorityApplication.transaction do
+        #   application.status = :submitted
+        #   application.update!(attributes)
+        #   SubmitToAppStore.new.process(submission: application)
+        # end
+      end
+    end
+  end
+end
