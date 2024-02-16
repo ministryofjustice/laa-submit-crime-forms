c3c7a589873cf9251990aed8fcc2a148b2351e5d
David Henry <david.henry@digital.justice.gov.uk>
2024-02-16 10:06:13 +0000

    remove greater_than_0 check from time periods

--- gems/laa_multi_step_forms/app/validators/time_period_validator.rb
+++ gems/laa_multi_step_forms/app/validators/time_period_validator.rb
@@ -31,8 +31,8 @@ class TimePeriodValidator < ActiveModel::EachValidator
     validate_minutes(time_period.minutes)
 
     if time_period.is_a?(IntegerTimePeriod)
-      add_error(:invalid_period) unless time_period.to_i >= 0
       add_error(:zero_time_period) if time_period.to_i.zero? && !options[:allow_zero]
+      add_error(:invalid_period) unless time_period.to_i >= 0
     else
       # If, after all, we still don't have a valid date object, it means
       # there are additional errors, like June 31st, or day 29 in non-leap year.
