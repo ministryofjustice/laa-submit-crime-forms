a4f746da246c41a387b060f09fec160c731bfa24
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-02-28 10:44:18 +0000

    Add nonces and CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,22 +4,26 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+
+    # If there are any un-nonceable scripts that need to be added to the page, add their SHAs here in the format
+    # policy.script_src  "'sha256-<sha 1 here>'", "'sha256-<sha 2 here>'", :self, :https, :report_sample
+    policy.script_src  :self, :https, :report_sample
+
+    # If there are any un-nonceable styles that need to be added to the page, add their SHAs here in the format
+    # policy.style_src  "'sha256-<sha 1 here>'", "'sha256-<sha 2 here>'", :self, :https, :report_sample
+    policy.style_src :self, :https, :report_sample
+
+    # Sentry creates workers from "blobs" in order to report on errors, so we allow that
+    policy.worker_src :blob
+  end
+
+  # Generate session nonces for permitted importmap and inline scripts
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
+end
