785e3f403909084b61a915a95fcbb1154a8f9263
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-03-11 12:31:50 +0000

    I was looking at the wrong bit of Miro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_provider!, only: :dev_login
   skip_before_action :can_access_service, only: :dev_login
-  layout 'nscc'
+  layout 'submit_a_crime_form'
 
   def dev_login; end
 end
