4b9958e88eb46a99a8c712cc166552409589e3c3
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-19 10:21:51 +0000

    Use form to calculate additional cost toal

--- app/models/additional_cost.rb
+++ app/models/additional_cost.rb
@@ -2,10 +2,6 @@ class AdditionalCost < ApplicationRecord
   belongs_to :prior_authority_application
 
   def total_cost
-    if unit_type == 'per_hour'
-      (cost_per_hour * period / 60)
-    elsif unit_type == 'per_item'
-      (cost_per_item * items)
-    end
+    PriorAuthority::Steps::AdditionalCosts::DetailForm.build(self, application: prior_authority_application).total_cost
   end
 end
