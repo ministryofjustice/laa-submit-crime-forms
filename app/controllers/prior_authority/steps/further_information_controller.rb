058e28ca627dcc0b512d91853a3818b16c78a9cd
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-20 15:50:05 +0000

    add basic controller, view, route for page

---
+++ app/controllers/prior_authority/steps/further_information_controller.rb
@@ -0,0 +1,19 @@
+module PriorAuthority
+  module Steps
+    class FurtherInformationController < BaseController
+      def edit
+
+      end
+
+      def update
+
+      end
+
+      private
+
+      def as
+        :further_information
+      end
+    end
+  end
+end
