4c8613b0d91e0fbc1403e7ac60650b60454066cc
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-21 11:52:41 +0000

    Add columns for showing most recent send back

---
+++ db/migrate/20240321114806_add_send_back_info.rb
@@ -0,0 +1,6 @@
+class AddSendBackInfo < ActiveRecord::Migration[7.1]
+  def change
+    add_column :prior_authority_applications, :further_information_explanation, :string
+    add_column :prior_authority_applications, :incorrect_information_explanation, :string
+  end
+end
