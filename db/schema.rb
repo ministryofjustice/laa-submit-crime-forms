6855f2434bd65a9c5404cfeaa9bb3f4441310b7f
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-21 14:58:41 +0000

    Add RFI fields to PA application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_18_102807) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_145242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -218,6 +218,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_102807) do
     t.string "custom_prison_name"
     t.datetime "app_store_updated_at"
     t.string "assessment_comment"
+    t.string "further_information_explanation"
+    t.string "incorrect_information_explanation"
     t.index ["firm_office_id"], name: "index_prior_authority_applications_on_firm_office_id"
     t.index ["provider_id"], name: "index_prior_authority_applications_on_provider_id"
     t.index ["solicitor_id"], name: "index_prior_authority_applications_on_solicitor_id"
