d81e98f61b10e7b5ca451e5e2b8e3ddcfdb346d3
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-26 17:20:46 +0000

    create tests for further info form

---
+++ spec/forms/prior_authority/steps/further_information_form_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe PriorAuthority::Steps::FurtherInformationForm do
+  subject(:form) { described_class.new(arguments) }
+
+  let(:arguments) do
+    {
+      application:,
+      record:,
+    }
+  end
+
+  describe '#validate' do
+    let(:application) { instance_double(PriorAuthorityApplication) }
+
+    context 'with information supplied' do
+      let(:record) { instance_double(FurtherInformation, information_supplied: 'some information') }
+
+      it 'test' do
+        binding.pry
+        expect(form).to be_valid
+      end
+    end
+
+    context 'with blank information supplied' do
+      let(:record) { instance_double(FurtherInformation, information_supplied: '') }
+      it 'has a validation error on the field' do
+        expect(form).not_to be_valid
+        expect(form.errors.of_kind?(:information_supplied, :blank)).to be(true)
+        expect(form.errors.messages[:information_supplied]).to include('Enter the requested information')
+      end
+    end
+  end
+
+  # describe '#save' do
+  #   subject(:save) { form.save }
+
+  #   let(:application) { create(:prior_authority_application) }
+
+  #   context 'with a valid reason why' do
+  #     let(:further_information) {
+  #       {
+  #         information_supplied: 'some information'
+  #       }
+  #     }
+
+  #     it 'persists the reason why' do
+  #       expect { save }.to change { application.reload.reason_why }.from(nil).to('important_stuff')
+  #     end
+  #   end
+  # end
+end
