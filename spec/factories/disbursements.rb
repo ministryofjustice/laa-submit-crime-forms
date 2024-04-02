1fced6ec4d12f0a07cedf93540909eebe357fd37
David Henry <david.henry@digital.justice.gov.uk>
2024-04-02 15:11:46 +0100

    lint warning

--- spec/factories/disbursements.rb
+++ spec/factories/disbursements.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     trait :valid do
       disbursement_date { age.days.ago.to_date }
       disbursement_type { DisbursementTypes.values.reject(&:other?).sample.to_s }
-      miles { 50 + 200**Random.new.rand(1.0).to_i } # range: 50 -> 250
+      miles { 50 + (200**Random.new.rand(1.0).to_i) } # range: 50 -> 250
       total_cost_without_vat do |disb|
         Pricing.for(Claim.new)[disb.disbursement_type] * disb.miles
       end
