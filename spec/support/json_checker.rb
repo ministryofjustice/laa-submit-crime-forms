c2da0b5530920ffa5d54b287da80b0718638622d
David Henry <david.henry@digital.justice.gov.uk>
2024-04-02 15:15:57 +0100

    explain Json_checker

--- spec/support/json_checker.rb
+++ spec/support/json_checker.rb
@@ -1,5 +1,8 @@
-# TODO: convert this to a matcher instead - if possible
+# Using this allows for easier diff checking as the `have_received` matcher puts individual line diff
+# instead of just the entire blob. This greatly speeds up the fixing process when something has been
+# changed
 module JsonChecker
+  # TODO: convert this to a matcher instead - if possible
   def check_json(payload)
     Checker.new(self, payload)
   end
