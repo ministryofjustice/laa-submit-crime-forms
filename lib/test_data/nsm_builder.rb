f9ed7c65241e820fb6b55df21ca1f61df07f9263
David Henry <david.henry@digital.justice.gov.uk>
2024-04-04 12:56:35 +0100

    Fix 0 work items bug

--- lib/test_data/nsm_builder.rb
+++ lib/test_data/nsm_builder.rb
@@ -20,6 +20,7 @@ module TestData
 
           # help with debugging for base tasks run (for simple objects):
           #   errors_for(task_klass, claim)
+          #   errors_for(task_klass, claim, record)
 
           raise "Invalid for #{invalid_tasks.map(&:first).join(', ')}"
         end
@@ -28,9 +29,9 @@ module TestData
       end
     end
 
-    def errors_for(task_klass, claim)
+    def errors_for(task_klass, claim, record = nil)
       task = task_klass.new(application: claim)
-      form = task.send(:associated_form).build(task.send(:record), application: claim)
+      form = task.send(:associated_form).build(record || task.send(:record), application: claim)
       form.valid?
       form.errors
     end
@@ -49,19 +50,19 @@ module TestData
       {
         magistrates: [
           [:claim, :complete, :case_type_magistrates, :build_associates],
-          proc { { disbursements_count: rand(25), work_items_count: rand(50) } }
+          proc { { disbursements_count: rand(25), work_items_count: rand(1..50) } }
         ],
         breach: [
           [:claim, :complete, :case_type_breach, :build_associates],
-          proc { { disbursements_count: rand(25), work_items_count: rand(50) } }
+          proc { { disbursements_count: rand(25), work_items_count: rand(1..50) } }
         ],
         no_disburesments: [
           [:claim, :complete, :case_type_magistrates, :build_associates],
-          proc { { disbursements_count: 0, work_items_count: rand(50) } }
+          proc { { disbursements_count: 0, work_items_count: rand(1..50) } }
         ],
         enhanced_rates: [
           [:claim, :complete, :case_type_magistrates, :with_enhanced_rates, :build_associates],
-          proc { { disbursements_count: rand(25), work_items_count: rand(50) } }
+          proc { { disbursements_count: rand(25), work_items_count: rand(1..50) } }
         ],
       }
     end
