10e215bce49b73d54d7e14be462f75c9a61e1a61
David Henry <david.henry@digital.justice.gov.uk>
2024-04-16 15:18:14 +0100

    Crm457 1282/pa data (#706)
    ## Description of change
    Create tests data for PA application
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1282)

---
+++ spec/support/be_one_off.rb
@@ -0,0 +1,9 @@
+RSpec::Matchers.define(:be_one_of) do |*expected|
+  match do |actual|
+    expected.include?(actual)
+  end
+
+  failure_message do |actual|
+    "expected one of #{expected}, got #{actual}"
+  end
+end
