ffcbb000ba64b7f4768fd4bbd27eed8d74f91c1d
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-04-18 16:17:21 +0100

    Feature flag postal evidence UI and tweak copy

--- spec/system/nsm/supporting_evidence_spec.rb
+++ spec/system/nsm/supporting_evidence_spec.rb
@@ -3,33 +3,53 @@ require 'rails_helper'
 RSpec.describe 'User can provide supporting evidence', type: :system do
   let(:claim) { create(:claim, :complete) }
 
-  it 'does not show the mail address' do
-    visit provider_saml_omniauth_callback_path
+  context 'when postal evidence feature is enabled' do
+    before do
+      allow(FeatureFlags).to receive(:postal_evidence).and_return(double(:postal_evidence, enabled?: true))
+    end
 
-    visit edit_nsm_steps_supporting_evidence_path(claim.id)
+    it 'does not show the mail address' do
+      visit provider_saml_omniauth_callback_path
 
-    element = find_by_id('nsm-steps-supporting-evidence-form-send-by-post-true-field', visible: :all)
+      visit edit_nsm_steps_supporting_evidence_path(claim.id)
 
-    expect(element).not_to be_checked
+      element = find_by_id('nsm-steps-supporting-evidence-form-send-by-post-true-field', visible: :all)
 
-    click_on 'Save and continue'
+      expect(element).not_to be_checked
 
-    expect(claim.reload).to have_attributes(send_by_post: false)
+      click_on 'Save and continue'
+
+      expect(claim.reload).to have_attributes(send_by_post: false)
+    end
+
+    it 'does shows the mail address' do
+      visit provider_saml_omniauth_callback_path
+
+      visit edit_nsm_steps_supporting_evidence_path(claim.id)
+
+      element = find_by_id('nsm-steps-supporting-evidence-form-send-by-post-true-field', visible: :all)
+
+      element.click
+
+      expect(element).to be_checked
+
+      click_on 'Save and continue'
+
+      expect(claim.reload).to have_attributes(send_by_post: true)
+    end
   end
 
-  it 'does shows the mail address' do
-    visit provider_saml_omniauth_callback_path
+  context 'when postal evidence feature is disabled' do
+    before do
+      allow(FeatureFlags).to receive(:postal_evidence).and_return(double(:postal_evidence, enabled?: false))
+    end
 
-    visit edit_nsm_steps_supporting_evidence_path(claim.id)
+    it 'does not show the mail address' do
+      visit provider_saml_omniauth_callback_path
 
-    element = find_by_id('nsm-steps-supporting-evidence-form-send-by-post-true-field', visible: :all)
+      visit edit_nsm_steps_supporting_evidence_path(claim.id)
 
-    element.click
-
-    expect(element).to be_checked
-
-    click_on 'Save and continue'
-
-    expect(claim.reload).to have_attributes(send_by_post: true)
+      expect(page).to have_no_css('#nsm-steps-supporting-evidence-form-send-by-post-true-field')
+    end
   end
 end
