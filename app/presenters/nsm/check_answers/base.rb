121271d81ebf236790562a4a41ca669c5b4c6649
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-04-26 12:45:26 +0100

    CRM457-1295: NSM CYA new cost summary card and layout tweaks (#714)
    ## Description of change
    - Use the new cost summary table on CYA page
    - Change width logic of other cards
    - Add LAA reference to top card
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1295)

--- app/presenters/nsm/check_answers/base.rb
+++ app/presenters/nsm/check_answers/base.rb
@@ -28,8 +28,7 @@ module Nsm
         heading = head_opts[:text] || translate_table_key(section, head_key, **head_opts)
         row = {
           key: {
-            text: heading,
-            classes: 'govuk-summary-list__value-width-50'
+            text: heading
           },
           value: {
             text:
@@ -64,6 +63,14 @@ module Nsm
 
       def currency_value(value)
         NumberTo.pounds(value || 0)
+      end
+
+      def custom
+        nil
+      end
+
+      def change_link_controller_method
+        :edit
       end
     end
   end
