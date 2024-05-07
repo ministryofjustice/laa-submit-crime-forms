c103d9a5188c603b329c5de02568724b4c3c3796
David Henry <david.henry@digital.justice.gov.uk>
2024-05-07 10:35:37 +0100

    Crm457 1394/status card (#746)
    ## Description of change
    Status screen based on status
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1394)
    
    ## Notes for reviewer
    The locations that rely on adjusted amounts have been left with a
    placeholder for now

--- spec/presenters/nsm/check_answers/application_status_card_spec.rb
+++ spec/presenters/nsm/check_answers/application_status_card_spec.rb
@@ -9,24 +9,21 @@ RSpec.describe Nsm::CheckAnswers::ApplicationStatusCard do
     let(:claim) { build(:claim) }
 
     it 'shows correct title' do
-      expect(subject.title).to eq('Status')
+      expect(subject.title).to eq('Claim status')
     end
   end
 
   describe '#row data' do
     context 'submitted' do
-      let(:claim) { build(:claim, :completed_status, :updated_at) }
+      let(:claim) { create(:claim, :completed_status, :firm_details, :build_associates, :updated_at, work_items_count: 1) }
 
       it 'generates submitted rows' do
-        expect(subject.row_data).to eq(
+        expect(subject.row_data).to match(
           [
             {
               head_key: 'application_status',
-              text: '<strong class="govuk-tag govuk-tag--primary">Submitted</strong>'
-            },
-            {
-              head_key: 'submitted',
-              text: '1 December 2023'
+              text: Regexp.new('<p><strong class="govuk-tag govuk-tag--primary">Submitted</strong></p>' \
+                               '<p>1 December 2023</p><p>Awaiting LAA review</p><br><p>£\d+\.\d\d claimed</p>')
             }
           ]
         )
@@ -34,18 +31,18 @@ RSpec.describe Nsm::CheckAnswers::ApplicationStatusCard do
     end
 
     context 'granted' do
-      let(:claim) { build(:claim, :granted_status) }
+      let(:claim) { create(:claim, :granted_status, :firm_details, :build_associates, :updated_at, work_items_count: 1) }
 
       it 'generates granted rows' do
-        expect(subject.row_data).to eq(
+        expect(subject.row_data).to match(
           [
             {
               head_key: 'application_status',
-              text: '<strong class="govuk-tag govuk-tag--green">Granted</strong>'
+              text: Regexp.new('<p><strong class="govuk-tag govuk-tag--green">Granted</strong></p>' \
+                               '<p>1 December 2023</p><br><p>£(\d+\.\d\d) claimed</p><p>£\1 allowed</p>')
             },
             {
-              head_key: 'assessed',
-              text: nil
+              head_key: 'laa_response', text: '<p>Fake LAA Response</p><p>Second line</p>'
             }
           ]
         )
@@ -53,37 +50,85 @@ RSpec.describe Nsm::CheckAnswers::ApplicationStatusCard do
     end
 
     context 'part granted' do
-      let(:claim) { build(:claim, :part_granted_status) }
+      let(:claim) do
+        create(:claim, :part_granted_status, :firm_details, :build_associates, :updated_at, work_items_count: 1)
+      end
 
       it 'generates part granted rows' do
-        expect(subject.row_data).to eq(
+        expect(subject.row_data).to match(
           [
             {
               head_key: 'application_status',
-              text: '<strong class="govuk-tag govuk-tag--blue">Part Granted</strong>'
+              text: Regexp.new('<p><strong class="govuk-tag govuk-tag--blue">Part Granted</strong></p>' \
+                               '<p>1 December 2023</p><br><p>£\d+\.\d\d claimed</p><p>Pending Data</p>')
             },
             {
-              head_key: 'assessed',
-              text: nil
+              head_key: 'laa_response',
+              text: '<p>Fake LAA Response</p><p>Second line</p>' \
+                    '<p><ul class="govuk-list govuk-list--bullet">' \
+                    '<li><a class="govuk-link govuk-link--no-visited-state" href="/non-standard-magistrates/applications/' \
+                    "#{claim.id}/steps/view_claim?section=adjustments#work_items\">" \
+                    'Review adjustments to work items</a></li><li><a class="govuk-link govuk-link--no-visited-state" ' \
+                    "href=\"/non-standard-magistrates/applications/#{claim.id}/steps/view_claim?" \
+                    'section=adjustments#letters_and_calls">Review adjustments to letters and calls</a></li>' \
+                    '<li><a class="govuk-link govuk-link--no-visited-state" href="/non-standard-magistrates/applications/' \
+                    "#{claim.id}/steps/view_claim?section=adjustments#disbursements\">Review adjustments " \
+                    'to disbursements</a></li></ul></p><p><a class="govuk-button govuk-!-margin-bottom-0" data-module="govuk-button" ' \
+                    "href=\"/non-standard-magistrates/applications/#{claim.id}/steps/view_claim\">" \
+                    'How to appeal this decision</a></p>'
             }
           ]
         )
       end
+
+      context 'no work items' do
+        let(:claim) { create(:claim, :part_granted_status, :firm_details, :updated_at) }
+
+        it 'generates part granted rows' do
+          expect(subject.row_data).to match(
+            [
+              {
+                head_key: 'application_status',
+                text: Regexp.new('<p><strong class="govuk-tag govuk-tag--blue">Part Granted</strong></p>' \
+                                 '<p>1 December 2023</p><br><p>£\d+\.\d\d claimed</p><p>Pending Data</p>')
+              },
+              {
+                head_key: 'laa_response',
+                text: '<p>Fake LAA Response</p><p>Second line</p>' \
+                      '<p><ul class="govuk-list govuk-list--bullet">' \
+                      '<li><a class="govuk-link govuk-link--no-visited-state" ' \
+                      "href=\"/non-standard-magistrates/applications/#{claim.id}/steps/view_claim?" \
+                      'section=adjustments#letters_and_calls">Review adjustments to letters and calls</a></li>' \
+                      '<li><a class="govuk-link govuk-link--no-visited-state" href="/non-standard-magistrates/' \
+                      "applications/#{claim.id}/steps/view_claim?section=adjustments#disbursements\">Review " \
+                      'adjustments to disbursements</a></li></ul></p><p>' \
+                      '<a class="govuk-button govuk-!-margin-bottom-0" data-module="govuk-button" ' \
+                      "href=\"/non-standard-magistrates/applications/#{claim.id}/steps/view_claim\">How to appeal " \
+                      'this decision</a></p>'
+              }
+            ]
+          )
+        end
+      end
     end
 
     context 'review' do
-      let(:claim) { build(:claim, :review_status) }
+      let(:claim) { create(:claim, :review_status, :firm_details, :build_associates, :updated_at, work_items_count: 1) }
 
       it 'generates review rows' do
-        expect(subject.row_data).to eq(
+        expect(subject.row_data).to match(
           [
             {
               head_key: 'application_status',
-              text: '<strong class="govuk-tag govuk-tag--yellow">Further Information Requested</strong>'
+              text: Regexp.new('<p><strong class="govuk-tag govuk-tag--yellow\">Update needed</strong></p><p>1 December 2023</p>' \
+                               '<br><p>£\d+\.\d\d claimed</p>')
+
             },
             {
-              head_key: 'returned',
-              text: nil
+              head_key: 'laa_response',
+              text: '<p>Fake LAA Response</p><p>Second line</p><p><span class="govuk-!-font-weight-bold">Update your claim</span>' \
+                    '</p><p>To update your claim, email <a href="mailto:magsbilling@justice.gov.uk">magsbilling@justice.gov.uk</a> ' \
+                    "with the LAA case reference in the subject of the email and include any supporting information requested.\n</p>"
             }
           ]
         )
@@ -91,18 +136,24 @@ RSpec.describe Nsm::CheckAnswers::ApplicationStatusCard do
     end
 
     context 'further info' do
-      let(:claim) { build(:claim, :further_info) }
+      let(:claim) do
+        create(:claim, :further_info_status, :firm_details, :build_associates, :updated_at, work_items_count: 1)
+      end
 
       it 'generates further info rows' do
-        expect(subject.row_data).to eq(
+        expect(subject.row_data).to match(
           [
             {
               head_key: 'application_status',
-              text: '<strong class="govuk-tag govuk-tag--yellow">Further Information Requested</strong>'
+              text: Regexp.new('<p><strong class="govuk-tag govuk-tag--yellow\">Update needed</strong></p><p>1 December 2023</p>' \
+                               '<br><p>£\d+\.\d\d claimed</p>')
+
             },
             {
-              head_key: 'returned',
-              text: nil
+              head_key: 'laa_response',
+              text: '<p>Fake LAA Response</p><p>Second line</p><p><span class="govuk-!-font-weight-bold">Update your claim</span>' \
+                    '</p><p>To update your claim, email <a href="mailto:magsbilling@justice.gov.uk">magsbilling@justice.gov.uk</a> ' \
+                    "with the LAA case reference in the subject of the email and include any supporting information requested.\n</p>"
             }
           ]
         )
@@ -110,18 +161,24 @@ RSpec.describe Nsm::CheckAnswers::ApplicationStatusCard do
     end
 
     context 'provider requested' do
-      let(:claim) { build(:claim, :provider_requested) }
+      let(:claim) do
+        create(:claim, :provider_requested_status, :firm_details, :build_associates, :updated_at, work_items_count: 1)
+      end
 
       it 'generates provider requested rows' do
-        expect(subject.row_data).to eq(
+        expect(subject.row_data).to match(
           [
             {
               head_key: 'application_status',
-              text: '<strong class="govuk-tag govuk-tag--yellow">Further Information Requested</strong>'
+              text: Regexp.new('<p><strong class="govuk-tag govuk-tag--yellow">Provider request</strong></p>' \
+                               '<p>1 December 2023</p><br><p>£\d+\.\d\d claimed</p>')
             },
             {
-              head_key: 'returned',
-              text: nil
+              head_key: 'laa_response',
+              text: '<p>Fake LAA Response</p><p>Second line</p><p><span class="govuk-!-font-weight-bold">Update your ' \
+                    'claim</span></p><p>To update your claim, email <a href="mailto:magsbilling@justice.gov.uk">' \
+                    'magsbilling@justice.gov.uk</a> with the LAA case reference in the subject of the email and include ' \
+                    "your new information.\n</p>"
             }
           ]
         )
@@ -129,18 +186,25 @@ RSpec.describe Nsm::CheckAnswers::ApplicationStatusCard do
     end
 
     context 'rejected' do
-      let(:claim) { build(:claim, :rejected_status) }
+      let(:claim) do
+        create(:claim, :rejected_status, :firm_details, :build_associates, :updated_at, work_items_count: 1)
+      end
 
       it 'generates rejected rows' do
-        expect(subject.row_data).to eq(
+        expect(subject.row_data).to match(
           [
             {
               head_key: 'application_status',
-              text: '<strong class="govuk-tag govuk-tag--red">Rejected</strong>'
+              text: Regexp.new('<p><strong class="govuk-tag govuk-tag--red">Rejected</strong></p><p>1 December 2023</p>' \
+                               '<br><p>£\d+\.\d\d claimed</p><p>£0.00 allowed</p>')
+
             },
             {
-              head_key: 'returned',
-              text: nil
+              head_key: 'laa_response',
+              text: '<p>Fake LAA Response</p><p>Second line</p>' \
+                    '<p><a class="govuk-button govuk-!-margin-bottom-0" data-module="govuk-button" ' \
+                    "href=\"/non-standard-magistrates/applications/#{claim.id}/steps/view_claim\">" \
+                    'How to appeal this decision</a></p>'
             }
           ]
         )
