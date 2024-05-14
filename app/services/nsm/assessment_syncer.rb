505ce996d5a49df42d6aae8c21a68fbe13355b25
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-05-14 15:42:25 +0100

    add code to sync letter adjustments

--- app/services/nsm/assessment_syncer.rb
+++ app/services/nsm/assessment_syncer.rb
@@ -12,7 +12,15 @@ module Nsm
     end
 
     def call
-      sync_overall_comment
+      case claim.status
+      when 'submitted', 'granted'
+        return
+      when 'part_grant'
+        sync_overall_comment
+        sync_letter_adjustments
+      when 'provider_requested', 'further_info'
+        sync_overall_comment
+      end
     rescue StandardError => e
       Sentry.capture_message("#{self.class.name} encountered error '#{e}' for claim '#{claim.id}'")
     end
@@ -20,12 +28,20 @@ module Nsm
     private
 
     def sync_overall_comment
-      return if claim.status.in?(%w[submitted granted])
-
       event_type = claim.provider_requested? || claim.further_info? ? 'send_back' : 'decision'
       comment_event = app_store_record['events'].select { _1['event_type'] == event_type }
                                                 .max_by { DateTime.parse(_1['created_at']) }
       claim.update(assessment_comment: comment_event.dig('details', 'comment'))
+    end
+
+    def sync_letter_adjustments
+      claim.update(allowed_letters: letters['count']) if letters['count_original'].present?
+      claim.update(allowed_letters_uplift: letters['uplift']) if letters['uplift_original'].present?
+      claim.update(letters_adjustment_comment: letters['adjustment_comment']) if letters['adjustment_comment'].present?
+    end
+
+    def letters
+      app_store_record['application']['letters_and_calls'].select { _1['type']['value'] == 'letters'}
     end
   end
 end
