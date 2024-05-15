968c712c6cfb0ab5df9635e0077bc25cd5fecd32
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-05-15 11:35:04 +0100

    Updates to uplift_applied? method

--- app/services/risk_assessment/high_risk_assessment.rb
+++ app/services/risk_assessment/high_risk_assessment.rb
@@ -29,7 +29,7 @@ module RiskAssessment
     end
 
     def uplift_applied?
-      @items[:work_items].work_item_forms.any? { _1.uplift&.positive? }
+      @items[:work_items].work_item_forms.any?(&:apply_uplift)
     end
 
     def extradition?
