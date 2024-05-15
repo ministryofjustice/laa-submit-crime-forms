77cf40eb7141791cbdc13b80b5a221ed8c08285e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-05-15 11:34:57 +0100

    add syncing of call adjustments

--- app/services/nsm/assessment_syncer.rb
+++ app/services/nsm/assessment_syncer.rb
@@ -15,6 +15,7 @@ module Nsm
       case claim.status
       when 'part_grant'
         sync_letter_adjustments
+        sync_call_adjustments
         sync_overall_comment
       when 'provider_requested', 'further_info', 'rejected'
         sync_overall_comment
@@ -33,9 +34,17 @@ module Nsm
     end
 
     def sync_letter_adjustments
-      update_letters_count(letters)
-      update_letters_uplift(letters)
-      claim.update(letters_adjustment_comment: letters['adjustment_comment']) if letters['adjustment_comment'].present?
+      record = letters
+      update_letters_count(record)
+      update_letters_uplift(record)
+      claim.update(letters_adjustment_comment: record['adjustment_comment']) if record['adjustment_comment'].present?
+    end
+
+    def sync_call_adjustments
+      record = calls
+      update_calls_count(record)
+      update_calls_uplift(record)
+      claim.update(calls_adjustment_comment: record['adjustment_comment']) if record['adjustment_comment'].present?
     end
 
     def letters
@@ -55,6 +64,25 @@ module Nsm
 
       claim.update(allowed_letters_uplift: letters['uplift'],
                    letters_uplift: letters['uplift_original'])
+    end
+
+    def calls
+      app_store_record['application']['letters_and_calls'].select { _1['type']['value'] == 'calls' }
+                                                          .first
+    end
+
+    def update_calls_count(calls)
+      return if calls['count_original'].blank?
+
+      claim.update(allowed_calls: calls['count'],
+                   calls: calls['count_original'])
+    end
+
+    def update_calls_uplift(calls)
+      return if calls['uplift_original'].blank?
+
+      claim.update(allowed_calls_uplift: calls['uplift'],
+                   calls_uplift: calls['uplift_original'])
     end
   end
 end
