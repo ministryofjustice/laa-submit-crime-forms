3dbbac7e7b91645b4a69f703d35b0538bd7dd92c
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-05-15 14:28:07 +0100

    Don't include adjustment fields in work item payload

--- app/services/submit_to_app_store/nsm_payload_builder.rb
+++ app/services/submit_to_app_store/nsm_payload_builder.rb
@@ -1,6 +1,7 @@
 class SubmitToAppStore
   class NsmPayloadBuilder
     DEFAULT_IGNORE = %w[claim_id created_at updated_at].freeze
+    WORK_ITEM_IGNORE = %[allowed_uplift allowed_time_spent].freeze
 
     attr_reader :claim, :scorer
 
@@ -63,7 +64,7 @@ class SubmitToAppStore
 
     def work_item_data
       claim.work_items.map do |work_item|
-        data = work_item.as_json(except: DEFAULT_IGNORE)
+        data = work_item.as_json(except: DEFAULT_IGNORE.concat(WORK_ITEM_IGNORE))
         data['completed_on'] = data['completed_on'].to_s
         data['pricing'] = pricing[work_item.work_type]
         data
