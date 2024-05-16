023059bf6d4c1155fbe1e5d931a7c36319fc2e4f
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-05-16 12:15:06 +0100

    Adds real data to see risk is calculated correctly

--- spec/services/risk_assessment/risk_assessment_scorer_spec.rb
+++ spec/services/risk_assessment/risk_assessment_scorer_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe RiskAssessment::RiskAssessmentScorer do
+  let(:fee_earner_initial) { Faker::Alphanumeric.alphanumeric(number: 2, min_alpha: 2, min_numeric: 0) }
+
   describe '#calculate' do
     context 'returns high risk' do
       let(:claim) { build(:claim, :with_assigned_council) }
@@ -13,7 +15,35 @@ RSpec.describe RiskAssessment::RiskAssessmentScorer do
     end
 
     context 'returns medium risk' do
-      let(:claim) { build(:claim, :medium_risk_work_item) }
+      let(:claim) do
+        build(:claim,
+              letters: 19,
+              calls: 1,
+              prosecution_evidence: 50,
+              defence_statement: 1,
+              number_of_witnesses: 1,
+              time_spent: 24,
+              assigned_counsel: 'no',
+              reasons_for_claim: [ReasonForClaim::CORE_COSTS_EXCEED_HIGHER_LMTS.to_s],
+              work_items: [
+                build(:work_item, work_type: WorkTypes::PREPARATION,
+                  time_spent: '23',
+                  completed_on: Time.zone.today,
+                  fee_earner: fee_earner_initial),
+                build(:work_item, work_type: WorkTypes::ATTENDANCE_WITHOUT_COUNSEL,
+                  time_spent: '40',
+                  completed_on: Time.zone.yesterday,
+                  fee_earner: fee_earner_initial),
+                build(:work_item, work_type: WorkTypes::ADVOCACY,
+                  time_spent: '26',
+                  completed_on: Time.zone.yesterday,
+                  fee_earner: fee_earner_initial),
+                build(:work_item, work_type: WorkTypes::TRAVEL,
+                  time_spent: '156',
+                  completed_on: Time.zone.yesterday,
+                  fee_earner: fee_earner_initial),
+              ])
+      end
 
       it do
         expect(described_class.calculate(claim)).to eq('medium')
@@ -21,7 +51,35 @@ RSpec.describe RiskAssessment::RiskAssessmentScorer do
     end
 
     context 'returns low risk' do
-      let(:claim) { build(:claim) }
+      let(:claim) do
+        build(:claim,
+              letters: 19,
+              calls: 1,
+              prosecution_evidence: 50,
+              defence_statement: 1,
+              number_of_witnesses: 1,
+              time_spent: 24,
+              assigned_counsel: 'no',
+              reasons_for_claim: [ReasonForClaim::CORE_COSTS_EXCEED_HIGHER_LMTS.to_s],
+              work_items: [
+                build(:work_item, work_type: WorkTypes::PREPARATION,
+                  time_spent: '552',
+                  completed_on: Time.zone.today,
+                  fee_earner: fee_earner_initial),
+                build(:work_item, work_type: WorkTypes::ATTENDANCE_WITHOUT_COUNSEL,
+                  time_spent: '282',
+                  completed_on: Time.zone.yesterday,
+                  fee_earner: fee_earner_initial),
+                build(:work_item, work_type: WorkTypes::ADVOCACY,
+                  time_spent: '246',
+                  completed_on: Time.zone.yesterday,
+                  fee_earner: fee_earner_initial),
+                build(:work_item, work_type: WorkTypes::TRAVEL,
+                  time_spent: '156',
+                  completed_on: Time.zone.yesterday,
+                  fee_earner: fee_earner_initial),
+              ])
+      end
 
       it do
         expect(described_class.calculate(claim)).to eq('low')
