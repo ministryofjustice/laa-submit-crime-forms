38b637b1436ac61ce2731ea8827c1ccb59746bf7
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-05-21 11:57:20 +0100

    update MD5 hash from new names

--- gems/laa_multi_step_forms/spec/models/court_spec.rb
+++ gems/laa_multi_step_forms/spec/models/court_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe LaaMultiStepForms::Court, type: :model do
     subject { described_class.all }
 
     it 'returns required courts as expected' do
-      digest_of_expected_court_names = '4cdc39a4fa9cfe2aeb4984fe1dbab5d1'
+      digest_of_expected_court_names = '288c887f6a9965d43e9c6b8766424d5f'
 
       expect(Digest::MD5.hexdigest(subject.map(&:name).join)).to eq digest_of_expected_court_names
     end
