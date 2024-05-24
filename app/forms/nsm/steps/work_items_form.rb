6d2d68e1e86e00b12e16fcbc66c6c96b0b1641c5
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-05-24 15:00:05 +0100

    CRM457-1492: Incomplete work item messaging (#826)
    ## Description of change
    Better messaging for incomplete work items
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1492)

--- app/forms/nsm/steps/work_items_form.rb
+++ app/forms/nsm/steps/work_items_form.rb
@@ -1,6 +1,18 @@
 module Nsm
   module Steps
     class WorkItemsForm < ::Steps::AddAnotherForm
+      def save
+        unless all_work_items_valid?
+          errors.add(:add_another, :invalid_item)
+          return false
+        end
+
+        super
+      end
+
+      def all_work_items_valid?
+        application.work_items.all? { WorkItemForm.build(_1, application:).valid? }
+      end
     end
   end
 end
