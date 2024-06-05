c8156e7b697a2aa2d6772da66b70e613093a2700
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-06-05 17:02:13 +0100

    associate application to alternative quote in factory

--- spec/factories/prior_authority_application.rb
+++ spec/factories/prior_authority_application.rb
@@ -125,11 +125,12 @@ FactoryBot.define do
       further_informations { [build(:further_information, :with_response, :with_supporting_documents)] }
       after(:build) do |paa|
         build(:quote, :primary, prior_authority_application_id: paa.id)
-        build(:quote, :alternative, document: nil)
+        build(:quote, :alternative, document: nil, prior_authority_application_id: paa.id)
       end
+
       after(:create) do |paa|
         create(:quote, :primary, prior_authority_application_id: paa.id)
-        create(:quote, :alternative, document: nil)
+        create(:quote, :alternative, document: nil, prior_authority_application_id: paa.id)
       end
     end
 
