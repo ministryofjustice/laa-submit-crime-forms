123bfb8ee19894c38e598e1f73089c862b9e1cbb
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-06-06 11:41:19 +0100

    Broaden high risk assessment and refactor unit tests

--- spec/services/risk_assessment/high_risk_assessment_spec.rb
+++ spec/services/risk_assessment/high_risk_assessment_spec.rb
@@ -4,88 +4,67 @@ require 'rails_helper'
 
 RSpec.describe RiskAssessment::HighRiskAssessment do
   describe '#assess' do
-    context 'returns true' do
-      context 'when cost is over £5000' do
-        let(:high_cost_claim) { create(:claim, :high_cost_disbursement) }
+    subject(:assessment) { described_class.new(claim).assess }
 
-        it do
-          expect(described_class.new(high_cost_claim)).to be_high_cost
-          expect(described_class.new(high_cost_claim).assess).to be_truthy
-        end
-      end
+    let(:claim) { create(:claim, :one_work_item) }
 
-      context 'when there is an assigned counsel' do
-        let(:assigned_council_claim) { build(:claim, :with_assigned_council) }
+    it 'returns false when no clauses are triggered' do
+      expect(assessment).to be_falsey
+    end
 
-        it do
-          expect(described_class.new(assigned_council_claim)).to be_assigned_counsel
-          expect(described_class.new(assigned_council_claim).assess).to be_truthy
-        end
-      end
+    context 'when cost is over £5000' do
+      before { create(:disbursement, :valid_high_cost, claim:) }
 
-      context 'when enhanced rates are claimed' do
-        let(:enhanced_rates_claim) { build(:claim, :with_enhanced_rates) }
-
-        it do
-          expect(described_class.new(enhanced_rates_claim)).to be_enhancement_applied
-          expect(described_class.new(enhanced_rates_claim).assess).to be_truthy
-        end
-      end
-
-      context 'when there is an uplift' do
-        let(:uplifted_claim) { build(:claim, :uplifted_work_item) }
-
-        it do
-          expect(described_class.new(uplifted_claim)).to be_uplift_applied
-          expect(described_class.new(uplifted_claim).assess).to be_truthy
-        end
-      end
-
-      context 'when there is an extradition' do
-        let(:extradition_claim) { build(:claim, :with_extradition) }
-
-        it do
-          expect(described_class.new(extradition_claim)).to be_extradition
-          expect(described_class.new(extradition_claim).assess).to be_truthy
-        end
-      end
-
-      context 'when a rep order is withdrawn' do
-        let(:withdrawn_reporder_claim) { build(:claim, :with_rep_order_withdrawn) }
-
-        it do
-          expect(described_class.new(withdrawn_reporder_claim)).to be_rep_order_withdrawn
-          expect(described_class.new(withdrawn_reporder_claim).assess).to be_truthy
-        end
+      it 'returns true' do
+        expect(assessment).to be_truthy
       end
     end
 
-    context 'returns false' do
-      let(:claim) { build(:claim, :one_work_item) }
+    context 'when there is an assigned counsel' do
+      before { claim.assigned_counsel = 'yes' }
 
-      it 'when cost is under £5000' do
-        expect(described_class.new(claim)).not_to be_high_cost
+      it 'returns true' do
+        expect(assessment).to be_truthy
       end
+    end
 
-      it 'when there is not an assigned counsel' do
-        expect(described_class.new(claim)).not_to be_assigned_counsel
+    context 'when there is an unassigned counsel' do
+      before { claim.unassigned_counsel = 'yes' }
+
+      it 'returns true' do
+        expect(assessment).to be_truthy
       end
+    end
 
-      it 'when enhanced rates are not claimed' do
-        expect(described_class.new(claim)).not_to be_enhancement_applied
+    context 'when there is an instructed agent' do
+      before { claim.agent_instructed = 'yes' }
+
+      it 'returns true' do
+        expect(assessment).to be_truthy
       end
+    end
 
-      it 'when there is not an uplift' do
-        expect(described_class.new(claim)).not_to be_uplift_applied
-        expect(described_class.new(claim).assess).not_to be_truthy
+    context 'when enhanced rates are claimed' do
+      before { claim.reasons_for_claim = [ReasonForClaim::ENHANCED_RATES_CLAIMED.to_s] }
+
+      it 'returns true' do
+        expect(assessment).to be_truthy
       end
+    end
 
-      it 'when there is not an extradition' do
-        expect(described_class.new(claim)).not_to be_extradition
+    context 'when there is an extradition' do
+      before { claim.reasons_for_claim = [ReasonForClaim::EXTRADITION.to_s] }
+
+      it 'returns true' do
+        expect(assessment).to be_truthy
       end
+    end
 
-      it 'when a rep order is not withdrawn' do
-        expect(described_class.new(claim)).not_to be_rep_order_withdrawn
+    context 'when a rep order is withdrawn' do
+      before { claim.reasons_for_claim = [ReasonForClaim::REPRESENTATION_ORDER_WITHDRAWN.to_s] }
+
+      it 'returns true' do
+        expect(assessment).to be_truthy
       end
     end
   end
