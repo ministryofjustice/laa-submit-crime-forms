fadcf32aa723d8a09c2670f2dca9d4343131d815
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-06-07 10:07:35 +0100

    remove index function

--- app/controllers/errors/inactive_offices_controller.rb
+++ app/controllers/errors/inactive_offices_controller.rb
@@ -2,9 +2,5 @@ module Errors
   class InactiveOfficesController < ApplicationController
     skip_before_action :authenticate_provider!
     skip_before_action :can_access_service
-
-    def index
-      render 'errors/inactive_offices/index'
-    end
   end
 end
