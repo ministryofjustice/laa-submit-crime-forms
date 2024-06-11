f474007fdcd5f9d69f79fade62f91ebd182717d7
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-06-11 11:44:38 +0100

    Add CSRF token to ajax calls
    Instead of skipping csrf authentication token checks
    this adds the csrf token check to all ajax calls. This
    is also more secure.

--- app/javascript/file-upload.js
+++ app/javascript/file-upload.js
@@ -133,3 +133,10 @@ $(function() {
     }
 })
 
+$(function(){
+  // always pass csrf tokens on ajax calls
+  $.ajaxSetup({
+    headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') }
+  });
+});
+
