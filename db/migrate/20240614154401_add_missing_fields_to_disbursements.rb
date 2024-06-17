24a30349d5c39deab57752fc910559b714a4a921
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-06-17 16:24:00 +0100

    only migrate if not exists

--- db/migrate/20240614154401_add_missing_fields_to_disbursements.rb
+++ db/migrate/20240614154401_add_missing_fields_to_disbursements.rb
@@ -1,6 +1,6 @@
 class AddMissingFieldsToDisbursements < ActiveRecord::Migration[7.1]
   def change
-    add_column :disbursements, :allowed_miles, :decimal, precision: 10, scale: 3
-    add_column :disbursements, :allowed_apply_vat, :string
+    add_column :disbursements, :allowed_miles, :decimal, precision: 10, scale: 3, if_not_exists: true
+    add_column :disbursements, :allowed_apply_vat, :string, if_not_exists: true
   end
 end
