08e4a85dd0fe064a7bb54f8acf17338a64452d2c
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-06-19 11:30:04 +0100

    Add sidekiq metrics instrumentation
    see https://github.com/discourse/prometheus_exporter?tab=readme-ov-file#sidekiq-metrics
    
    Notes:
    
    - Prevent sidekiq workers attempt to start prometheus puma instrumentation
    
      Causes errors as below
    
      ```
      E, [2024-06-21T14:59:17.147753 #1] ERROR -- : PrometheusExporter::Instrumentation::Puma Prometheus Exporter Failed To Collect Stats undefined method `stats' for nil
      ```

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,45 +1,60 @@
 # The below is heavily cribbed from ministryofjustice/laa-apply-for-criminal-legal-aid
 return unless ENV.fetch("ENABLE_PROMETHEUS_EXPORTER", "false") == "true"
 
+require 'prometheus_exporter/client'
 require "prometheus_exporter/server"
+require "prometheus_exporter/instrumentation"
+require "prometheus_exporter/middleware"
 
 DEFAULT_PREFIX = "ruby_".freeze
+DEFAULT_BIND_ADDRESS = "0.0.0.0".freeze
+DEFAULT_PORT = 9394
 
 # We are running puma in single process mode, so this is safe
 # If we move to multi process mode, we will have to run the
 # exporter process separately (`bundle exec prometheus_exporter`)
 def start_prometheus_server
   server = PrometheusExporter::Server::WebServer.new(
-    bind: "0.0.0.0", port: ENV.fetch("PROMETHEUS_EXPORTER_PORT", 9394).to_i,
+    bind: ENV.fetch("PROMETHEUS_EXPORTER_HOST", DEFAULT_BIND_ADDRESS),
+    port: ENV.fetch("PROMETHEUS_EXPORTER_PORT", DEFAULT_PORT).to_i,
     verbose: ENV.fetch("PROMETHEUS_EXPORTER_VERBOSE", "false") == "true"
   )
 
   server.start
-
-  true
+  server
 rescue Errno::EADDRINUSE
   warn "[PrometheusExporter] Server port already in use."
   false
 end
 
-return unless start_prometheus_server
+Rails.logger.info("[PrometheusExporter] Starting server....")
+server = start_prometheus_server
 
-require "prometheus_exporter/instrumentation"
-require "prometheus_exporter/middleware"
+Rails.logger.info "[PrometheusExporter] server #{JSON.parse(server.to_json).fetch_values('port', 'bind')}...."
+return unless server
 
-Rails.logger.info "[PrometheusExporter] Initialising instrumentation middleware..."
+PrometheusExporter::Client.default = PrometheusExporter::LocalClient.new(collector: server.collector)
+
+Rails.logger.info("[PrometheusExporter] Initialising standard instrumentation middleware...")
 
 # Metrics will be prefixed, for example `ruby_http_requests_total`
 PrometheusExporter::Metric::Base.default_prefix = DEFAULT_PREFIX
 
 # This reports stats per request like HTTP status and timings
+#
 Rails.application.middleware.unshift PrometheusExporter::Middleware
 
 # This reports basic process stats like RSS and GC info, type master
-# means it is instrumenting the master process
+# means it is instrumenting the master process.
+# see config/puma.rb  after_work_boot for more
+#
 PrometheusExporter::Instrumentation::Process.start(type: "master")
 
-# NOTE: if running Puma in cluster mode, the following
-# instrumentation will need to be changed
-PrometheusExporter::Instrumentation::Puma.start unless PrometheusExporter::Instrumentation::Puma.started?
-PrometheusExporter::Instrumentation::ActiveRecord.start
+# NOTE: if running Puma in cluster mode, the following instrumentation will need to be
+# moved to an after_worker_boot block in puma config.
+# NOTE: attempting to instrument puma prom export on sidekiq servers will result in lots of error logs
+#
+unless Sidekiq.server?
+  PrometheusExporter::Instrumentation::Puma.start unless PrometheusExporter::Instrumentation::Puma.started?
+  PrometheusExporter::Instrumentation::ActiveRecord.start
+end
