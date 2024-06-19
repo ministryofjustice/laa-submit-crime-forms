3140f3cf64db748363edcc7a83dc22a5caae2133
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-06-19 11:30:04 +0100

    Add sidekiq metrics instrumentation
    see https://github.com/discourse/prometheus_exporter?tab=readme-ov-file#sidekiq-metrics
    
    Notes:
    
    - Prevent sidekiq workers attempt to start prometheus puma instrumentation
    
      Causes errors as below
    
      ```
      E, [2024-06-21T14:59:17.147753 #1] ERROR -- : PrometheusExporter::Instrumentation::Puma Prometheus Exporter Failed To Collect Stats undefined method `stats' for nil
      ```

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -25,18 +25,42 @@ redis_url = Rails.configuration.x.redis_url
 
 module Dashboard; end
 
+Rails.logger.info("[Sidekiq] Application config initialising...")
+
 Sidekiq.configure_client do |config|
+  Rails.logger.info("[SidekiqClient] configuring sidekiq client...")
   config.logger.level = Logger::WARN if Rails.env.test?
   config.redis = { url: redis_url } if redis_url
 end
 
 Sidekiq.configure_server do |config|
+  Rails.logger.info("[SidekiqServer] configuring sidekiq server...")
   config.redis = { url: redis_url } if redis_url
-end
 
-SidekiqPrometheus.configure do |config|
-  config.preset_labels = { service: 'laa-submit-crime-forms' }
-  config.periodic_reporting_interval   = 15
-end
+  return unless ENV.fetch("ENABLE_PROMETHEUS_EXPORTER", "false") == "true"
 
-SidekiqPrometheus.setup
+  Rails.logger.info("[SidekiqPrometheusExporter] Instrumentation for sidekiq server...")
+  require "sidekiq/api"
+  require 'prometheus_exporter/client'
+  require 'prometheus_exporter/instrumentation'
+
+  # Taken from https://github.com/discourse/prometheus_exporter?tab=readme-ov-file#sidekiq-metrics
+  #
+  config.server_middleware do |chain|
+    Rails.logger.info "[SidekiqPrometheusExporter] Chaining middleware..."
+    chain.add PrometheusExporter::Instrumentation::Sidekiq
+  end
+  config.death_handlers << PrometheusExporter::Instrumentation::Sidekiq.death_handler
+  config.on :startup do
+    Rails.logger.info "[SidekiqPrometheusExporter] Startup instrumention details..."
+
+    PrometheusExporter::Instrumentation::Process.start type: 'sidekiq'
+    PrometheusExporter::Instrumentation::SidekiqProcess.start
+    PrometheusExporter::Instrumentation::SidekiqQueue.start(all_queues: true)
+    PrometheusExporter::Instrumentation::SidekiqStats.start
+  end
+
+  at_exit do
+    PrometheusExporter::Client.default.stop(wait_timeout_seconds: 10)
+  end
+end
