7600d07b575d13b7314914d2b864a04424c70a61
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-07-12 15:22:48 +0100

    Add comment to make clearer what's going on

--- spec/system/prior_authority/view_applications_spec.rb
+++ spec/system/prior_authority/view_applications_spec.rb
@@ -164,6 +164,7 @@ RSpec.describe 'View reviewed applications' do
             information_requested: 'Tell me more',
             information_supplied: 'More info',
             created_at: 1.day.ago,
+            # the first dash in the below file name in an en dash, which is not a valid ISO-8859-1 character
             supporting_documents: [build(:supporting_document, file_name: 'evidence–with-weird-char.pdf')])
     end
 
