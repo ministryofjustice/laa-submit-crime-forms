a4d3f4deae40b5096abeaabc7d190e6255970e91
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-07-16 09:26:57 +0100

    add comment describing why we add additional input

--- app/javascript/handle-forms.js
+++ app/javascript/handle-forms.js
@@ -11,6 +11,11 @@ $(function() {
       $(this).attr("disabled", true)
     })
 
+    /*
+      this code ensures the button's submission type is appended to params
+      so that the BaseStepController still has this available to know
+      whether the form should commit_draft/save_and_refresh/reload
+    */
     let buttonAction = $(this).attr("name")
     if(buttonAction){
       form.append(`<input type="hidden" name="${buttonAction}" value="" >`)
