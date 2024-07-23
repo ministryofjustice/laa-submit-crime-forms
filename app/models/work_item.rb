fe07b584945cec9f4a70ef006df414ca3ebed4e2
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-07-23 12:44:40 +0100

    Adds sorting for work items

--- app/models/work_item.rb
+++ app/models/work_item.rb
@@ -14,4 +14,19 @@ class WorkItem < ApplicationRecord
   def translated_work_type
     translations(work_type, 'nsm.steps.check_answers.show.sections.work_items')
   end
+
+  def sort_position
+    {
+      'travel' => 0,
+      'waiting' => 1,
+      'attendance_with_counsel' => 2,
+      'attendance_without_counsel' => 3,
+      'preparation' => 4,
+      'advocacy' => 5
+    }[work_type.to_s]
+  end
+
+  def position
+    1
+  end
 end
