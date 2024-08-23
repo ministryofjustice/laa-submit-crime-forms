367a3c4b571332481d534920c0adb931eec10cc8
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-08-23 17:19:49 +0100

    Try raising errors on redis failures

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,11 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: Rails.configuration.x.redis_url }
+  config.cache_store = :redis_cache_store, {
+    url: Rails.configuration.x.redis_url,
+    error_handler: -> (method:, returning:, exception:) { raise exception },
+  }
+
   config.session_store :cache_store, secure: true
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
