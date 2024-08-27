cb414c9ca6f1e8705f764ef860c62f55e5dd4fa1
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-08-27 16:15:08 +0100

    first revision of using notification banner in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
 
   def index
     @gatekeeper = Providers::Gatekeeper.new(current_provider)
+    @notification_banners = NotificationBanners.instance.banners
   end
 
   def dev_login; end
