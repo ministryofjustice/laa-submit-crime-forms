cbfa41efac330beac11cb429dbca27654ab02c60
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-08-27 19:21:48 +0100

    make safer

--- lib/notification_banner.rb
+++ lib/notification_banner.rb
@@ -1,18 +1,18 @@
 class NotificationBanner
   def self.active_banner
     current_date = DateTime.now
-    banner_config && date_from <= current_date && date_to >= current_date ? banner_config[:message] : nil
+    date_from <= current_date && date_to >= current_date ? banner_config[:message] : nil
   end
 
   def self.banner_config
-    Rails.configuration.x.notification_banner.notification || nil
+    Rails.configuration.x.notification_banner&.notification
   end
 
   def self.date_from
-    DateTime.parse(banner_config[:date_from]) || nil
+    banner_config ? DateTime.parse(banner_config[:date_from]) : nil
   end
 
   def self.date_to
-    DateTime.parse(banner_config[:date_to]) || nil
+    banner_config ? DateTime.parse(banner_config[:date_to]) : nil
   end
 end
