34bfa52cd3651ed31d9a3feaa3c639470bb945b6
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-09-25 17:09:24 +0100

    Simplify

--- app/models/matter_type.rb
+++ app/models/matter_type.rb
@@ -1,26 +1,7 @@
 MatterType = Struct.new(:id) do
-  # rubocop:disable Metrics/MethodLength
   def self.all
-    @all ||= [
-      new('1'),
-      new('2'),
-      new('3'),
-      new('4'),
-      new('5'),
-      new('6'),
-      new('7'),
-      new('8'),
-      new('9'),
-      new('10'),
-      new('11'),
-      new('12'),
-      new('13'),
-      new('14'),
-      new('15'),
-      new('16'),
-    ]
+    @all ||= (1..16).map { new(_1.to_s) }
   end
-  # rubocop:enable Metrics/MethodLength
 
   def description
     I18n.t("laa_crime_forms_common.nsm.matter_type.#{id}")
--- app/models/outcome_code.rb
+++ app/models/outcome_code.rb
@@ -1,32 +1,32 @@
 OutcomeCode = Struct.new(:id) do
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  # rubocop:disable Metrics/MethodLength
   def self.all
-    @all ||= [
-      new('CP01'),
-      new('CP02'),
-      new('CP03'),
-      new('CP04'),
-      new('CP05'),
-      new('CP06'),
-      new('CP07'),
-      new('CP08'),
-      new('CP09'),
-      new('CP10'),
-      new('CP11'),
-      new('CP12'),
-      new('CP13'),
-      new('CP16'),
-      new('CP17'),
-      new('CP18'),
-      new('CP19'),
-      new('CP20'),
-      new('CP21'),
-      new('CP22'),
-      new('CP23'),
-      new('CP24'),
-    ]
+    @all ||= %w[
+      CP01
+      CP02
+      CP03
+      CP04
+      CP05
+      CP06
+      CP07
+      CP08
+      CP09
+      CP10
+      CP11
+      CP12
+      CP13
+      CP16
+      CP17
+      CP18
+      CP19
+      CP20
+      CP21
+      CP22
+      CP23
+      CP24
+    ].map { new(_1) }
   end
-  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+  # rubocop:enable Metrics/MethodLength
 
   def description
     I18n.t("laa_crime_forms_common.nsm.hearing_outcome.#{id}")
