3fe515d31538a64455296477bcb6ab17113e3153
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-10-07 09:58:22 +0100

    use correct time format in test

--- spec/services/submit_to_app_store/nsm_payload_builder_spec.rb
+++ spec/services/submit_to_app_store/nsm_payload_builder_spec.rb
@@ -196,7 +196,7 @@ RSpec.describe SubmitToAppStore::NsmPayloadBuilder do
   end
 
   context 'when the claim is provider updated' do
-    let(:updated_at) { DateTime.current }
+    let(:updated_at) { DateTime.new(2024, 2, 2, 1, 1, 1) }
     let(:claim) do
       create(:claim, :complete, :with_further_information_supplied, state: 'provider_updated', updated_at: updated_at)
     end
@@ -207,7 +207,7 @@ RSpec.describe SubmitToAppStore::NsmPayloadBuilder do
           'id' => claim.id,
           'is_other_info' => 'no',
           'laa_reference' => 'LAA-n4AohV',
-          'updated_at' => updated_at,
+          'updated_at' => '2024-02-02T01:01:01.000Z',
           'status' => 'sent_back',
             'further_information' => [
               { 'caseworker_id' => '87e88ac6-d89a-4180-80d4-e03285023fb0',
