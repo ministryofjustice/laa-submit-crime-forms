42f5e1150be332dfe0349893eb808c2e2a1a7d34
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-10-07 11:08:27 +0100

    remove extra line from merge

--- app/services/nsm/assessment_syncer.rb
+++ app/services/nsm/assessment_syncer.rb
@@ -85,7 +85,6 @@ module Nsm
     def sync_further_info_requests
       data['further_information'].each do |further_info|
         record = claim.further_informations.find_or_create_by(
-        claim.further_informations.find_or_create_by(
           caseworker_id: further_info['caseworker_id'],
           information_requested: further_info['information_requested'],
           requested_at: further_info['requested_at']
