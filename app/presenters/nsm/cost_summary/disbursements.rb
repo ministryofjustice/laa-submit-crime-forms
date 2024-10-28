e7f61c1216139a8c87378e75f92d58eea2e78d50
Ellis Kenyo <ellis.kenyo@digital.justice.gov.uk>
2024-10-28 17:12:34 +0000

    CRM7: Add single summary row for disbursements

--- app/presenters/nsm/cost_summary/disbursements.rb
+++ app/presenters/nsm/cost_summary/disbursements.rb
@@ -20,7 +20,15 @@ module Nsm
       end
 
       def total_cost
-        @total_cost ||= disbursements.filter_map(&:total_cost_pre_vat).sum
+        @total_cost ||= disbursements.filter_map(&:total_cost).sum
+      end
+
+      def net_cost
+        @net_cost ||= disbursements.filter_map(&:total_cost_pre_vat).sum
+      end
+
+      def vat_amount
+        @vat_amount ||= disbursements.filter_map(&:vat_amount).sum
       end
 
       def title
@@ -38,9 +46,39 @@ module Nsm
         ]
       end
 
+      def total_cost_cell
+        safe_join([tag.span(translate('.header.total_claimed'), class: 'govuk-visually-hidden'),
+                   tag.strong(NumberTo.pounds(total_cost))])
+      end
+
+      def net_cost_cell
+        safe_join([tag.span(translate('.header.net_cost_claimed'), class: 'govuk-visually-hidden'),
+                   tag.strong(NumberTo.pounds(net_cost))])
+      end
+
+      def vat_amount_cell
+        safe_join([tag.span(translate('.header.vat_on_claimed'), class: 'govuk-visually-hidden'),
+                   tag.strong(NumberTo.pounds(vat_amount))])
+      end
+
+      def disbursement_summary_footer_row
+        [
+          [
+            { text: translate('disbursements'), classes: 'govuk-table__header' },
+            { text: net_cost_cell, classes: 'govuk-table__cell--numeric govuk-summary-list__value-bold' },
+            { text: vat_amount_cell, classes: 'govuk-table__cell--numeric govuk-summary-list__value-bold' },
+            { text: total_cost_cell, classes: 'govuk-table__cell--numeric govuk-summary-list__value-bold' }
+          ].compact
+        ]
+      end
+
       def disbursement_summary_header_row
         [
           { text: tag.span(translate('.header.item'), class: 'govuk-visually-hidden') },
+          { text: translate('.header.net_cost_claimed'),
+            classes: 'govuk-table__header--numeric' },
+          { text: translate('.header.vat_on_claimed'),
+            classes: 'govuk-table__header--numeric' },
           { text: translate('.header.total_claimed'),
             classes: 'govuk-table__header--numeric' },
         ]
