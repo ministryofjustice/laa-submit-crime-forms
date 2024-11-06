6d80ff29d72aecd73988d4465caecfda131287d0
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-11-06 11:46:54 +0000

    Fix issues

--- gems/laa_multi_step_forms/app/attributes/type/gbp.rb
+++ gems/laa_multi_step_forms/app/attributes/type/gbp.rb
@@ -4,7 +4,8 @@ module Type
       if suitable_for_casting?(value)
         # When a monetary value is provided by a form submission, we are only interested
         # in the value to 2 decimal places, and discard any more fine detail immediately
-        super(value&.to_s&.delete(',')&.delete('£'))&.round(2)
+        with_extraneous_removed = value.to_s&.delete(',')&.delete('£') if value
+        super(with_extraneous_removed)&.round(2)
       else
         # If the user has entered a string that is not straightforwardly parseable
         # as an integer, retain the original value so we can display it back to the user
