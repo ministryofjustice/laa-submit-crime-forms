373c954050046cca0dc419c0f16522f88dd7017c
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-11-20 14:57:31 +0000

    Removes the controller spec

--- spec/controllers/nsm/steps/youth_court_claim_additional_fee_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Nsm::Steps::EqualityController, type: :controller do
-  # it_behaves_like 'a generic step controller', Nsm::Steps::YouthCourtClaimAdditionalFeeForm, Decisions::DecisionTree
-  it_behaves_like 'a step that can be drafted', Nsm::Steps::YouthCourtClaimAdditionalFeeForm
-end
