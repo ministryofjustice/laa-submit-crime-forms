155fad12a8c3fda8ab208597b10ec103fa8c3c0e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-11-20 15:24:28 +0000

    add tests for CaseCategoryForm#choices

---
+++ spec/forms/nsm/steps/case_category_form_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe Nsm::Steps::CaseCategoryForm do
+  subject(:form) { described_class.new(arguments) }
+
+  let(:arguments) do
+    {
+      application:,
+      plea_category:,
+    }
+  end
+
+  let(:application) { create(:claim, claim_type:, youth_court:) }
+  let(:plea_category) { nil }
+  let(:youth_court) { 'yes' }
+  let(:claim_type) { 'non_standard_magistrate' }
+
+  describe '#choices' do
+    context 'when not breach of injunction claim type AND youth court hearing' do
+      let(:youth_court) { 'yes' }
+      let(:claim_type) { 'non_standard_magistrate' }
+
+      it 'returns the correct choices' do
+        expect(subject.choices).to eq(PleaCategory::WITH_YOUTH_COURT_OPTIONS)
+      end
+    end
+
+    context 'when not breach of injunction claim type AND not youth court hearing' do
+      let(:youth_court) { 'no' }
+      let(:claim_type) { 'non_standard_magistrate' }
+
+      it 'returns the correct choices' do
+        expect(subject.choices).to eq(PleaCategory::WITHOUT_YOUTH_COURT_OPTIONS)
+      end
+    end
+
+    context 'when breach of injunction claim type AND youth court hearing' do
+      let(:youth_court) { 'yes' }
+      let(:claim_type) { 'breach_of_injunction' }
+
+      it 'returns the correct choices' do
+        expect(subject.choices).to eq(PleaCategory::WITHOUT_YOUTH_COURT_OPTIONS)
+      end
+    end
+
+    context 'when breach of injunction claim type AND not youth court hearing' do
+      let(:youth_court) { 'no' }
+      let(:claim_type) { 'breach_of_injunction' }
+
+      it 'returns the correct choices' do
+        expect(subject.choices).to eq(PleaCategory::WITHOUT_YOUTH_COURT_OPTIONS)
+      end
+    end
+  end
+end
