8566fc21e672ce5d0d4713425a35cd5e31e037ac
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-11-20 17:23:56 +0000

    fix linting issues

--- app/helpers/radio_form_helper.rb
+++ app/helpers/radio_form_helper.rb
@@ -1,6 +1,6 @@
 module RadioFormHelper
-  def radio_legend(text)
-    heading = tag.h1(t('.heading'), class: %w( govuk-fieldset__heading ))
-    tag.legend(heading, class: %w(govuk-fieldset__legend govuk-fieldset__legend--l))
+  def radio_legend(_text)
+    heading = tag.h1(t('.heading'), class: %w[govuk-fieldset__heading])
+    tag.legend(heading, class: %w[govuk-fieldset__legend govuk-fieldset__legend--l])
   end
 end
--- spec/system/nsm/case_category_spec.rb
+++ spec/system/nsm/case_category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User can fill in case category', type: :system do
   let(:claim) { create(:claim, claim_type:, youth_court:) }
-  let(:claim_type) { 'non_standard_magistrate'}
+  let(:claim_type) { 'non_standard_magistrate' }
   let(:youth_court) { 'yes' }
 
   before do
@@ -22,7 +22,7 @@ RSpec.describe 'User can fill in case category', type: :system do
   end
 
   context 'plea category is not populated' do
-    let(:claim) { create(:claim, claim_type:, youth_court:, plea_category: nil) }
+    let(:claim) { create(:claim, claim_type: claim_type, youth_court: youth_court, plea_category: nil) }
 
     it 'shows error message' do
       visit edit_nsm_steps_case_category_path(claim.id)
@@ -31,5 +31,4 @@ RSpec.describe 'User can fill in case category', type: :system do
       expect(page).to have_content('Select the case category')
     end
   end
-
 end
