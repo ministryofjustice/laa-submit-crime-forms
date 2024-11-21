76694004fc7a07c8cf684307d096474c9149bb1d
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-11-21 16:01:14 +0000

    delete unnecessary helper

--- app/helpers/radio_form_helper.rb
+++
@@ -1,6 +0,0 @@
-module RadioFormHelper
-  def radio_legend(_text)
-    heading = tag.h1(t('.heading'), class: %w[govuk-fieldset__heading])
-    tag.legend(heading, class: %w[govuk-fieldset__legend govuk-fieldset__legend--l])
-  end
-end
