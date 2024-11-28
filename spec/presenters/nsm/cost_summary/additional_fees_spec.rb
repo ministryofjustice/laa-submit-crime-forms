f178231af8d83156aacc5445f189e3094335ff77
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-11-28 18:24:43 +0000

    Add tests for additional fees section

---
+++ spec/presenters/nsm/cost_summary/additional_fees_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Nsm::CostSummary::AdditionalFees do
+  subject { described_class.new(claim) }
+
+  let(:claim) do
+    instance_double(Claim, firm_office:, totals:)
+  end
+  let(:firm_office) { build(:firm_office, :valid) }
+  let(:totals) do
+    {
+      youth_court_fee: {
+        claimed_total_inc_vat: 718.31
+      },
+      total: {
+        claimed_total_inc_vat: 718.31
+      }
+    }
+  end
+
+  describe '#rows' do
+    it 'generates additional fee rows' do
+      expect(subject.rows).to eq(
+        [
+          [
+            { classes: 'govuk-table__header', text: 'Youth court fee' },
+            { classes: 'govuk-table__cell--numeric', text: '£718.31' }
+          ]
+        ]
+      )
+    end
+  end
+end
