be308c61373a289c2fbf8b6976e84a5cb34e533b
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-12-02 10:51:17 +0000

    update assessment syncer logic

--- app/services/nsm/assessment_syncer.rb
+++ app/services/nsm/assessment_syncer.rb
@@ -94,8 +94,10 @@ module Nsm
     end
 
     def sync_youth_court_fee_adjustment
-      claim.allowed_youth_court_fee = data['allowed_youth_court_fee']
-      claim.youth_court_fee_adjustment_comment = data['youth_court_fee_adjustment_comment']
+      if data['include_youth_court_fee_original'].present?
+        claim.allowed_youth_court_fee = data['include_youth_court_fee']
+        claim.youth_court_fee_adjustment_comment = data['youth_court_fee_adjustment_comment']
+      end
     end
 
     def letters
