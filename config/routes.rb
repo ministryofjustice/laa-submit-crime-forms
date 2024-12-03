40e3d0cd5a9893cc1cc2ab95e6e192d7e94984af
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-12-03 11:02:14 +0000

    Add additional pages and logic for claimed additional fees

--- config/routes.rb
+++ config/routes.rb
@@ -135,6 +135,7 @@ Rails.application.routes.draw do
               get "work_items", to: "view_claim#claimed_work_items", as: :claimed_costs_work_items
               get "letters_and_calls", to: "view_claim#claimed_letters_and_calls", as: :claimed_costs_letters_and_calls
               get "disbursements", to: "view_claim#claimed_disbursements", as: :claimed_costs_disbursements
+              get "additional_fees", to: "view_claim#claimed_additional_fees", as: :claimed_costs_additional_fees
             end
 
             scope "adjusted" do
@@ -148,6 +149,7 @@ Rails.application.routes.draw do
                               constraints: { item_type: /(work_item|disbursement)/ }
             get :letters, to: 'view_claim#item', defaults: { item_type: 'letters' }
             get :calls, to: 'view_claim#item', defaults: { item_type: 'calls' }
+            get :additional_fees, to: 'view_claim#item', defaults: { item_type: 'additional_fees' }
             get :download
           end
         end
