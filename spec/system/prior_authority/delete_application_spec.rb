5539a1400bc1aca0f4e45c355fa54ae95ec05355
Ellis Kenyő <ellis.kenyo@digital.justice.gov.uk>
2024-12-20 13:49:37 +0000

    Correct the behaviour of the delete application test
    We weren't properly asserting we were seeing the right page, just that
    we don't find the UFN for the application we tried to delete; which
    means we would ignore any error pages that flagged up.

--- spec/system/prior_authority/delete_application_spec.rb
+++ spec/system/prior_authority/delete_application_spec.rb
@@ -2,7 +2,7 @@ require 'system_helper'
 
 RSpec.describe 'Prior authority application deletion' do
   before do
-    fill_in_until_step(:your_application_progress)
+    fill_in_until_step(:submit_application)
     click_on 'Back to your applications'
     click_on 'Drafts'
   end
@@ -11,6 +11,8 @@ RSpec.describe 'Prior authority application deletion' do
     click_on 'Delete'
     expect(page).to have_content 'Are you sure you want to delete this draft application?'
     click_on 'Yes, delete it'
+
+    expect(page).to have_content I18n.t('.prior_authority.applications.destroy.deleted')
     expect(page).to have_no_content '111111/123'
   end
 
@@ -18,6 +20,8 @@ RSpec.describe 'Prior authority application deletion' do
     click_on 'Delete'
     expect(page).to have_content 'Are you sure you want to delete this draft application?'
     click_on 'No, do not delete it'
+
+    expect(page).to have_no_content I18n.t('.prior_authority.applications.destroy.deleted')
     expect(page).to have_content '111111/123'
   end
 end
