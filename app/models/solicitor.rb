d1f9b19ff02be029f9e7871760207d5f7990631e
Ellis Kenyő <ellis.kenyo@digital.justice.gov.uk>
2024-12-23 10:24:12 +0000

    Use correct model name for relationship

--- app/models/solicitor.rb
+++ app/models/solicitor.rb
@@ -1,6 +1,6 @@
 class Solicitor < ApplicationRecord
-  has_one :prior_authority_applications, dependent: :destroy
-  has_one :claims, dependent: :destroy
+  has_one :prior_authority_application, dependent: :destroy
+  has_one :claim, dependent: :destroy
 
   def contact_full_name
     "#{contact_first_name} #{contact_last_name}"
