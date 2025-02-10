0ff59c0476c4b54b108ddb344fe7c16990cd0f22
Ellis Kenyő <ellis.kenyo@digital.justice.gov.uk>
2025-02-10 13:44:22 +0000

    Introduce an XML schema
    See CRM457-2424 for additional context

--- spec/system/nsm/import_spec.rb
+++ spec/system/nsm/import_spec.rb
@@ -10,30 +10,30 @@ RSpec.describe 'Import claims' do
     attach_file(file_fixture('import_sample.xml'))
     click_on 'Save and continue'
     expect(page).to have_content(
-      'We imported 1 work item and 2 disbursements. ' \
+      'We imported 3 work items and 2 disbursements. ' \
       'You will need to enter all other details manually, and check carefully the imported details.'
     )
-    expect(WorkItem.first).to have_attributes(
-      work_type: 'travel',
-      time_spent: 300,
-      completed_on: Date.new(2013, 1, 21),
-      fee_earner: 'RT',
-      uplift: 100,
+    expect(WorkItem.find_by(position: 1)).to have_attributes(
+      work_type: 'preparation',
+      time_spent: 120,
+      completed_on: Date.new(2024, 1, 15),
+      fee_earner: 'JS',
+      uplift: 20,
       position: 1,
     )
     expect(Disbursement.find_by(miles: nil)).to have_attributes(
       disbursement_type: 'other',
-      other_type: 'DNA Testing',
-      total_cost_without_vat: 400.0,
-      details: 'These are details of DNA testing.',
+      other_type: 'psychiatric_reports',
+      total_cost_without_vat: 350.00,
+      details: 'Expert psychiatric assessment',
       apply_vat: 'true',
-      vat_amount: 80.0,
+      vat_amount: 70.00,
     )
     expect(Disbursement.find_by(disbursement_type: 'car')).to have_attributes(
-      miles: 12.0,
-      details: 'Mileage',
-      apply_vat: 'false',
-      vat_amount: nil,
+      miles: 45.5,
+      details: 'Travel to Liverpool Magistrates Court',
+      apply_vat: 'true',
+      vat_amount: 4.09,
     )
   end
 
@@ -46,9 +46,6 @@ RSpec.describe 'Import claims' do
   it 'handles unreadable files' do
     attach_file(file_fixture('unreadable_import.xml'))
     click_on 'Save and continue'
-    expect(page).to have_content(
-      'We were unable to read any work items or disbursements from this file. ' \
-      'Please try a different file or enter your details manually.'
-    )
+    expect(page).to have_content("ERROR: Element 'claim': Missing child element(s).")
   end
 end
