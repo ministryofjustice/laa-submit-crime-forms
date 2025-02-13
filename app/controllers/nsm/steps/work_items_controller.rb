8f14b3cadde661c3e9ec4357a6054728cfede65c
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-02-13 16:35:19 +0000

    fix generation of flash

--- app/controllers/nsm/steps/work_items_controller.rb
+++ app/controllers/nsm/steps/work_items_controller.rb
@@ -4,7 +4,7 @@ module Nsm
       before_action :set_default_table_sort_options
 
       def edit
-        build_items_incomplete_flash
+        @items_incomplete_flash = build_items_incomplete_flash
         @summary = CostSummary::WorkItems.new(current_application.work_items, current_application)
         @pagy, @work_items = pagy_array(Sorters::WorkItemsSorter.call(current_application.work_items, @sort_by, @sort_direction))
         @form_object = WorkItemsForm.build(
@@ -36,11 +36,7 @@ module Nsm
       end
 
       def build_items_incomplete_flash
-        if incomplete_work_item_summary.incomplete_items.blank?
-          @items_incomplete_flash ||= nil
-        else
-          @build_items_incomplete_flash ||= { default: incomplete_work_item_summary.summary }
-        end
+        incomplete_work_item_summary.incomplete_items.blank? ? nil : { default: incomplete_work_item_summary.summary }
       end
     end
   end
