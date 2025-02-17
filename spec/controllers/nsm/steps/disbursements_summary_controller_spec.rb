367a79b5107bf2ba57de3fdce8a5ded09f573d4f
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-02-17 10:03:50 +0000

    add additional controller level testing

--- spec/controllers/nsm/steps/disbursements_summary_controller_spec.rb
+++ spec/controllers/nsm/steps/disbursements_summary_controller_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Nsm::Steps::DisbursementsController, type: :controller do
+  context 'when some disbursements are incomplete' do
+    let(:claim) { create(:claim, disbursements:) }
+    let(:disbursements) do
+      [
+        build(:disbursement, :valid),
+        build(:disbursement, :valid, disbursement_date: nil),
+        build(:disbursement, :valid, disbursement_date: nil)
+      ]
+    end
+
+    before do
+      allow(controller).to receive(:current_application).and_return(claim)
+    end
+
+    it 'generates the correct error summary' do
+      get :edit, params: { id: claim.id }
+      expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
+        .to include('2 disbursements are incomplete:')
+    end
+  end
+
   it_behaves_like 'a generic step controller', Steps::AddAnotherForm, Decisions::DecisionTree
   it_behaves_like 'a step that can be drafted', Steps::AddAnotherForm
 end
--- spec/controllers/nsm/steps/work_items_summary_controller_spec.rb
+++ spec/controllers/nsm/steps/work_items_summary_controller_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Nsm::Steps::WorkItemsController, type: :controller do
+  context 'when some work items are incomplete' do
+    let(:claim) { create(:claim, work_items:) }
+    let(:work_items) do
+      [
+        build(:work_item, :valid),
+        build(:work_item, :valid, time_spent: nil),
+        build(:work_item, :valid, time_spent: nil)
+      ]
+    end
+
+    before do
+      allow(controller).to receive(:current_application).and_return(claim)
+    end
+
+    it 'generates the correct error summary' do
+      get :edit, params: { id: claim.id }
+      expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
+        .to include('2 work items are incomplete:')
+    end
+  end
+
   it_behaves_like 'a generic step controller', Steps::AddAnotherForm, Decisions::DecisionTree
   it_behaves_like 'a step that can be drafted', Steps::AddAnotherForm
 end
