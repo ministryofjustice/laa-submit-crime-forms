07fa357a65b50dceae503b2666f6915e66e6aaa6
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-02-21 11:25:58 +0000

    add additional tests for incomplete item links

--- spec/controllers/nsm/steps/disbursements_summary_controller_spec.rb
+++ spec/controllers/nsm/steps/disbursements_summary_controller_spec.rb
@@ -20,6 +20,17 @@ RSpec.describe Nsm::Steps::DisbursementsController, type: :controller do
       expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
         .to include('2 disbursements are incomplete:')
     end
+
+    it 'creates the correct link for incomplete disbursement' do
+      incomplete_disbursements = claim.disbursements.reject(&:complete?).sort_by(&:position)
+      get :edit, params: { id: claim.id }
+      expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
+        .to include("<a class=\"govuk-link\" href=\"/non-standard-magistrates/applications/#{claim.id}" \
+                    "/steps/disbursement_type/#{incomplete_disbursements[0].id}\">item 1</a>")
+      expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
+        .to include("<a class=\"govuk-link\" href=\"/non-standard-magistrates/applications/#{claim.id}" \
+                    "/steps/disbursement_type/#{incomplete_disbursements[1].id}\">item 2</a>")
+    end
   end
 
   it_behaves_like 'a generic step controller', Steps::AddAnotherForm, Decisions::DecisionTree
--- spec/controllers/nsm/steps/work_items_summary_controller_spec.rb
+++ spec/controllers/nsm/steps/work_items_summary_controller_spec.rb
@@ -20,6 +20,17 @@ RSpec.describe Nsm::Steps::WorkItemsController, type: :controller do
       expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
         .to include('2 work items are incomplete:')
     end
+
+    it 'creates the correct link for incomplete disbursement' do
+      incomplete_work_items = claim.work_items.reject(&:complete?).sort_by(&:position)
+      get :edit, params: { id: claim.id }
+      expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
+        .to include("<a class=\"govuk-link\" href=\"/non-standard-magistrates/applications/#{claim.id}" \
+                    "/steps/work_item/#{incomplete_work_items[0].id}\">item 1</a>")
+      expect(controller.instance_variable_get(:@items_incomplete_flash)[:default])
+        .to include("<a class=\"govuk-link\" href=\"/non-standard-magistrates/applications/#{claim.id}" \
+                    "/steps/work_item/#{incomplete_work_items[1].id}\">item 2</a>")
+    end
   end
 
   it_behaves_like 'a generic step controller', Steps::AddAnotherForm, Decisions::DecisionTree
