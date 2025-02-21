48bcefcb275f145936b56caf9f942f5f9f8e416a
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-02-21 10:13:43 +0000

    use br tag instead of closing <p> tag for newline

--- app/services/nsm/importers/xml/import_service.rb
+++ app/services/nsm/importers/xml/import_service.rb
@@ -43,7 +43,7 @@ module Nsm
         def message_content
           "#{I18n.t('nsm.imports.work_items_message', count: @claim.work_items.count)}
             #{I18n.t('nsm.imports.disbursements_message', count: @claim.disbursements.count)}
-            </p>#{I18n.t('nsm.imports.message_end')}"
+            <br>#{I18n.t('nsm.imports.message_end')}"
         end
 
         def schema
