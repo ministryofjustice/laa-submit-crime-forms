5f8f4f2738ac41a152466875d9dcab938cb1c6ba
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-04-23 16:28:45 +0100

    remove unnecessary error file association

--- app/models/failed_import.rb
+++ app/models/failed_import.rb
@@ -1,8 +1,3 @@
 class FailedImport < ApplicationRecord
   belongs_to :provider
-  has_one :error_file,
-          dependent: :destroy,
-          inverse_of: :documentable,
-          class_name: 'SupportingDocument',
-          as: :documentable
 end
