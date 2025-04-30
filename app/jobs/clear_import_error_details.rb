a3e094f7cae5e444ad923886cec60d97372ccb11
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-04-30 15:40:23 +0100

    remove scheduled job for clearing import error details

--- app/jobs/clear_import_error_details.rb
+++ app/jobs/clear_import_error_details.rb
@@ -1,16 +1,1 @@
-class ClearImportErrorDetails < ApplicationJob
-  sidekiq_options retry: 1
-
-  def perform
-    return false if filtered_records.empty?
-
-    filtered_records.each do |record|
-      record.details = nil
-      record.save
     end
-  end
-
-  def filtered_records
-    FailedImport.where(created_at: ..1.week.ago.end_of_day)
-  end
-end
