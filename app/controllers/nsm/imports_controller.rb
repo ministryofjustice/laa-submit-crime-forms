42b6974d8c8904ee8aea78eb953e7945eeceda0d
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-05-02 11:30:02 +0100

    fix syntax issue

--- app/controllers/nsm/imports_controller.rb
+++ app/controllers/nsm/imports_controller.rb
@@ -31,7 +31,7 @@ module Nsm
       error_id = params.fetch(:error_id)
       @error_object = AppStoreClient.new.get_import_error(error_id)
 
-      if error_object['details'].present?
+      if @error_object['details'].present?
         generate_error_download
       else
         render 'nsm/imports/missing_file'
