bb956dd18950ac8ce35b2750718e29f44cf367d2
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-05-13 16:13:20 +0100

    clean up handling of error instance variable

--- app/controllers/nsm/imports_controller.rb
+++ app/controllers/nsm/imports_controller.rb
@@ -29,10 +29,10 @@ module Nsm
 
     def errors
       error_id = params.fetch(:error_id)
-      @error_object = AppStoreClient.new.get_import_error(error_id)
+      error_object = AppStoreClient.new.get_import_error(error_id)
 
-      if @error_object['details'].present?
-        @error_object['parsed_details'] = JSON.parse(@error_object['details'])
+      if error_object['details'].present?
+        @errors = JSON.parse(error_object['details'])
         generate_error_download
       else
         render 'nsm/imports/missing_file'
