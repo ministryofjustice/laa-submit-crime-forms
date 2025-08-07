e5d3fff8daf27d3d9a93c8c582ca349ff53f6cf8
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-08-07 14:06:47 +0100

    fix new factories

--- spec/factories/app_store_claims.rb
+++ spec/factories/app_store_claims.rb
@@ -1,3 +1,5 @@
-factory :app_store_claim, class: AppStore::V1::Nsm::Claim, parent: :claim do
-  laa_reference { 'LAA-ABC123' }
+FactoryBot.define do
+  factory :app_store_claim, class: AppStore::V1::Nsm::Claim, parent: :claim do
+    laa_reference { 'LAA-ABC123' }
+  end
 end
--- spec/factories/app_store_prior_authority_application.rb
+++ spec/factories/app_store_prior_authority_application.rb
@@ -1,5 +1,7 @@
-factory :app_store_prior_authority_application,
-        class: AppStore::V1::PriorAuthority::Application,
-        parent: :prior_authority_application do
-  laa_reference { 'LAA-ABC123' }
+FactoryBot.define do
+  factory :app_store_prior_authority_application,
+          class: AppStore::V1::PriorAuthority::Application,
+          parent: :prior_authority_application do
+    laa_reference { 'LAA-ABC123' }
+  end
 end
