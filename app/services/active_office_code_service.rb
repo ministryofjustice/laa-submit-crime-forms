332fca0bd33c3819a54dfec8a03d29416be95ce4
Ellis Kenyő <ellis.kenyo@justice.gov.uk>
2025-08-15 15:35:41 +0100

    Adjust the logic to fallback to our static lists only if PDA is down
    - If PDA is down, check the lists we have. If it's not in the list,
    assume it's not active
    - If PDA is up, use the result we get back to determine

--- app/services/active_office_code_service.rb
+++ app/services/active_office_code_service.rb
@@ -7,11 +7,32 @@ class ActiveOfficeCodeService
     end
 
     def active?(office_code)
+      result = if FeatureFlags.provider_api_login_check.enabled?
+                 contract_active?(office_code)
+               else
+                 :unavailable
+               end
+
+      case result
+      when :active then true
+      when :inactive then false
+      when :unavailable then known_active_code?(office_code)
+      # Can't trigger this as contract_active? only returns on the
+      # the symbols above
+      # :nocov:
+      else
+        raise "Unexpected result #{result} when querying provider API for office codes"
+      end
+      # :nocov:
+    end
+
+    private
+
+    def known_active_code?(office_code)
       return true if always_active_office_codes.include?(office_code)
       return false if always_inactive_office_codes.include?(office_code)
-      return contract_active?(office_code) if FeatureFlags.provider_api_login_check.enabled?
 
-      true
+      false
     end
 
     def always_inactive_office_codes
