f8ca6b3ab96402d6a45b358173e284ba7e339af8
Ivan Yohuno <ivan.yohuno@digital.justice.gov.uk>
2025-08-22 12:44:26 +0000

    CRM457-2704: Force Cascade Deletion on Laa Reference to Remove Virtual Search Columns (#1591)
    ## Description of change
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-2704)
    
    [Thread](https://mojdt.slack.com/archives/C034YPREUSF/p1755855318680329)

--- db/migrate/20250805120900_remove_laa_reference.rb
+++ db/migrate/20250805120900_remove_laa_reference.rb
@@ -1,6 +1,6 @@
 class RemoveLaaReference < ActiveRecord::Migration[8.0]
   def change
-    remove_column :claims, :laa_reference, :string
-    remove_column :prior_authority_applications, :laa_reference, :string
+    remove_column :claims, :laa_reference, :string, force: :cascade
+    remove_column :prior_authority_applications, :laa_reference, :string, force: :cascade
   end
 end
