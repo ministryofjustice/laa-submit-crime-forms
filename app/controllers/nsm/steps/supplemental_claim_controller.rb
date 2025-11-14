b304607a91fa42235be240c1f95c4d375e02e507
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-11-14 14:33:16 +0000

    make back button record_id logic clearer

--- app/controllers/nsm/steps/supplemental_claim_controller.rb
+++ app/controllers/nsm/steps/supplemental_claim_controller.rb
@@ -2,7 +2,12 @@ module Nsm
   module Steps
     class SupplementalClaimController < ApplicationController
       def show
-        @record_id = params[:claim_id] == StartPage::NEW_RECORD ? StartPage::NEW_RECORD : params[:claim_id]
+        # @record_id is needed for the back button of this page
+        @record_id = if params[:claim_id] == StartPage::NEW_RECORD
+                       StartPage::NEW_RECORD
+                     else
+                       params[:claim_id]
+                     end
       end
     end
   end
