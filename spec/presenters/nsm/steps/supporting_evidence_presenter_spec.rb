bf056f051e08b839314d120e039bdba4eed6b159
Ivan Yohuno <ivan.yohuno@digital.justice.gov.uk>
2025-11-19 17:36:01 +0000

    CRM457-2812: Submissions may be incorrectly flagged for evidence deletion (#1676)
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-2812)
    
    ## Notes for reviewer
    
    - Set complete? for supporting evidence section to need both supporting
    evidence to be present AND gdpr_documents_deleted to be false
    - add tests

--- spec/presenters/nsm/steps/supporting_evidence_presenter_spec.rb
+++ spec/presenters/nsm/steps/supporting_evidence_presenter_spec.rb
@@ -4,12 +4,27 @@ RSpec.describe Nsm::Tasks::SupportingEvidence, type: :system do
   subject { described_class.new(application:) }
 
   let(:application) { create(:claim, attributes) }
-  let(:attributes) { { id: } }
+  let(:attributes) { { id:, gdpr_documents_deleted: } }
   let(:id) { SecureRandom.uuid }
+  let(:gdpr_documents_deleted) { false }
 
   describe '#path' do
     it { expect(subject.path).to eq("/non-standard-magistrates/applications/#{id}/steps/supporting_evidence") }
   end
 
-  it_behaves_like 'a task with generic complete?', Nsm::Steps::SupportingEvidenceForm
+  describe '#complete?' do
+    it { expect(subject.complete?).to be false }
+
+    context 'claim has supporting evidence' do
+      let(:application) { create(:claim, :with_evidence, attributes) }
+
+      it { expect(subject.complete?).to be true }
+
+      context 'claim has gdpr_documents_deleted as true' do
+        let(:gdpr_documents_deleted) { true }
+
+        it { expect(subject.complete?).to be false }
+      end
+    end
+  end
 end
