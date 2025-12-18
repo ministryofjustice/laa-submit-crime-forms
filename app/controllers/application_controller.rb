c1a838df621941409d172222109b0764dd63e4b0
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-12-18 14:26:56 +0000

    maintenance mode for bank holidays

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,21 @@ class ApplicationController < LaaMultiStepForms::ApplicationController
 
   def check_maintenance_mode
     return if Rails.env.test?
-    return unless !business_hours? || ENV.fetch('MAINTENANCE_MODE', 'false') == 'true'
+    return unless bank_holidays? || !business_hours? || ENV.fetch('MAINTENANCE_MODE', 'false') == 'true'
 
     render file: 'public/maintenance.html', layout: false
+  end
+
+  def bank_holidays?
+    special_ranges = [
+      (Date.new(2025, 12, 25)..Date.new(2025, 12, 26)),
+      Date.new(2026, 1, 1)
+    ]
+
+    date = Time.current.to_date
+    special_ranges.any? do |range_or_date|
+      range_or_date.is_a?(Range) ? range_or_date.cover?(date) : range_or_date == date
+    end
   end
 
   def business_hours?
