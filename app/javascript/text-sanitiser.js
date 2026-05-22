fe135984715c82a91d7aa9b5bbc1dc713b3cbd6c
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-05-22 14:12:43 +0100

    Fix text sanitising code

--- app/javascript/text-sanitiser.js
+++ app/javascript/text-sanitiser.js
@@ -1,22 +1,40 @@
 function processString(string){
     //replace new lines with <br> tags to preserve formatting when displaying the string in HTML
-    string.replace("/\n/g", '<br>')
-    return string
+    let new_string = string.replace(/\r?\n/g, '<br>')
+    return new_string
 }
+
+function revProcessString(string){
+    //replace <br> tags with new lines to preserve formatting when displaying the string in a text area
+    let new_string = string.replace(/<br>/g, '\n')
+    return new_string
+}
+
+const form = document.querySelector('form')
+const submitButton = form.querySelector('button[type="submit"]')
+const fields = form.querySelectorAll('textarea')
+
+//replace text value on page load with reverse formatting
+document.addEventListener('DOMContentLoaded', () => {
+    if (!fields) return;
+
+    fields.forEach(field => {
+        if (field?.value) {
+            field.value = revProcessString(field.value)
+        }
+    });
+});
+  
 
 //sanitize text area value before submitting the form
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form')
-  const submitButton = form.querySelector('button[type="submit"]')
-  const fields = form.querySelectorAll('textarea')
+    if (!fields || !submitButton) return;
 
-  if (!form || !submitButton || !fields) return;
-
-  submitButton.addEventListener('click', () => {
+    submitButton.addEventListener('click', () => {
     fields.forEach(field => {
-      if (field?.value) {
+        if (field?.value) {
         field.value = processString(field.value)
-      }
+        }
     });
   });
 });
