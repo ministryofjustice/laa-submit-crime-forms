ministryofjustice/laa-assess-crime-forms

CRM457-2512: Propagate request IDs to downstream calls

app/services/outbound_request_id.rb
@@ -1,0 +1,20 @@
+require 'request_store'
+
+class OutboundRequestId
+  STORE_KEY = :outbound_request_id
+  SERVICE_PREFIX = 'nscc-assess'.freeze
+
+  class << self
+    def set(request_id)
+      RequestStore.store[STORE_KEY] = request_id.to_s if request_id.present?
+    end
+
+    def current
+      RequestStore.store[STORE_KEY].presence || "#{SERVICE_PREFIX}-#{SecureRandom.uuid}"
+    end
+
+    def headers
+      { 'request-id' => current }
+    end
+  end
+end

spec/services/outbound_request_id_spec.rb
@@ -1,0 +1,29 @@
+require 'rails_helper'
+require 'request_store'
+
+RSpec.describe OutboundRequestId do
+  after { RequestStore.clear! }
+
+  describe '.current' do
+    it 'returns the request id stored for the current request' do
+      described_class.set('rails-request-id')
+
+      expect(described_class.current).to eq('rails-request-id')
+    end
+
+    it 'generates a unique NSCC Assess id when no request id is stored' do
+      allow(SecureRandom).to receive(:uuid).and_return('first-uuid', 'second-uuid')
+
+      expect(described_class.current).to eq('nscc-assess-first-uuid')
+      expect(described_class.current).to eq('nscc-assess-second-uuid')
+    end
+  end
+
+  describe '.headers' do
+    it 'returns a request-id header' do
+      described_class.set('rails-request-id')
+
+      expect(described_class.headers).to eq('request-id' => 'rails-request-id')
+    end
+  end
+end

ministryofjustice/laa-submit-crime-forms

app/services/outbound_request_id.rb
@@ -1,0 +1,20 @@
+require 'request_store'
+
+class OutboundRequestId
+  STORE_KEY = :outbound_request_id
+  SERVICE_PREFIX = 'nscc-submit'.freeze
+
+  class << self
+    def set(request_id)
+      RequestStore.store[STORE_KEY] = request_id.to_s if request_id.present?
+    end
+
+    def current
+      RequestStore.store[STORE_KEY].presence || "#{SERVICE_PREFIX}-#{SecureRandom.uuid}"
+    end
+
+    def headers
+      { 'request-id' => current }
+    end
+  end
+end

spec/services/outbound_request_id_spec.rb
@@ -1,0 +1,29 @@
+require 'rails_helper'
+require 'request_store'
+
+RSpec.describe OutboundRequestId do
+  after { RequestStore.clear! }
+
+  describe '.current' do
+    it 'returns the request id stored for the current request' do
+      described_class.set('rails-request-id')
+
+      expect(described_class.current).to eq('rails-request-id')
+    end
+
+    it 'generates a unique NSCC Submit id when no request id is stored' do
+      allow(SecureRandom).to receive(:uuid).and_return('first-uuid', 'second-uuid')
+
+      expect(described_class.current).to eq('nscc-submit-first-uuid')
+      expect(described_class.current).to eq('nscc-submit-second-uuid')
+    end
+  end
+
+  describe '.headers' do
+    it 'returns a request-id header' do
+      described_class.set('rails-request-id')
+
+      expect(described_class.headers).to eq('request-id' => 'rails-request-id')
+    end
+  end
+end